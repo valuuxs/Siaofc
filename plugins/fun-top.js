9220426fb566ce89436a30778c43f908fbbdae6a
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-14 21:30:30 -0500

    Actualizar fun-top.js

--- plugins/fun-top.js
+++ plugins/fun-top.js
@@ -36,6 +36,7 @@ handler.help = ['top *<texto>*']
 handler.command = ['top']
 handler.tags = ['fun']
 handler.group = true
+handler.register = true
 export default handler
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]}
