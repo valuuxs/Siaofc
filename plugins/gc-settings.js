64c6a9e5b0ed1df57fc9f9d1db056ce517bd1693
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-14 21:45:58 -0500

    Actualizar gc-settings.js

--- plugins/gc-settings.js
+++ plugins/gc-settings.js
@@ -7,8 +7,8 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         return conn.reply(`*[ 🔐 ] Elija una opción.*\n\n*${usedPrefix + command}* abrir\n${usedPrefix + command}* cerrar`, m, rcanal)
     await conn.groupSettingUpdate(m.chat, isClose)
 }
-handler.help = ['group *abrir/cerrar*']
-handler.tags = ['group']
+handler.help = ['group *<abrir/cerrar>*']
+handler.tags = ['gc']
 handler.command = ['group', 'grupo'] 
 handler.admin = true
 handler.botAdmin = true
