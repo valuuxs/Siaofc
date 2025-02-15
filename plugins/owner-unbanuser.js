5011f17264add8ae5805534417a96f9b0e8df900
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-14 21:55:27 -0500

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -20,7 +20,7 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
         await conn.reply(m.chat, `🚩 El usuario no está registrado.`, m);
     }
 };
-handler.help = ['unbanuser <@tag>'];
+handler.help = ['unbanuser *<@tag>*'];
 handler.command = ['unbanuser'];
 handler.tags = ['owner'];
 handler.prems = true;
