f94d8d3c2a38af3ae4fa66404f94e6702a404214
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-14 23:00:29 -0500

    Actualizar plugins

---
+++ plugins/buscador-tiktoksearch.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+const { proto, generateWAMessageFromContent, generateWAMessageContent } = (await import("@whiskeysockets/baileys")).default;
+
+let handler = async (message, { conn, text, usedPrefix, command }) => {
+  if (!text) return conn.reply(message.chat, '*[ ☕ ] Por favor, ingrese el texto de lo que desea buscar en tiktok.*', message);
+
+  // Función para crear el mensaje de video
+  async function createVideoMessage(url) {
+    const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer });
+    return videoMessage;
+  }
+
+  try {
+    // Mensaje inicial informando al usuario que se está descargando el video
+    conn.reply(message.chat, '*Espere un momento...*', message);
+
+    // Realizar la búsqueda de TikTok
+    let { data: response } = await axios.get('https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=' + text);
+    let searchResults = response.data;
+
+    // Limitar a los primeros 7 resultados
+    let selectedResults = searchResults.slice(0, 7);
+
+    // Crear el mensaje con los resultados
+    let results = [];
+    for (let result of selectedResults) {
+      results.push({
+        body: proto.Message.InteractiveMessage.Body.fromObject({ text: null }),
+        footer: proto.Message.InteractiveMessage.Footer.fromObject({ text: 'Shadow Bot - MD' }),
+        header: proto.Message.InteractiveMessage.Header.fromObject({
+          title: result.title,
+          hasMediaAttachment: true,
+          videoMessage: await createVideoMessage(result.nowm)
+        }),
+        nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] })
+      });
+    }
+
+    // Crear el mensaje con el carrusel de resultados
+    const responseMessage = generateWAMessageFromContent(message.chat, {
+      viewOnceMessage: {
+        message: {
+          messageContextInfo: {
+            deviceListMetadata: {},
+            deviceListMetadataVersion: 2
+          },
+          interactiveMessage: proto.Message.InteractiveMessage.fromObject({
+            body: proto.Message.InteractiveMessage.Body.create({ text: '*☕ Resultado de:* ' + text }),
+            footer: proto.Message.InteractiveMessage.Footer.create({ text: 'TikTok - Search' }),
+            header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
+            carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [...results] })
+          })
+        }
+      }
+    }, { quoted: message });
+
+    // Enviar el mensaje de respuesta
+    await conn.relayMessage(message.chat, responseMessage.message, { messageId: responseMessage.key.id });
+
+  } catch (error) {
+    await conn.reply(message.chat, error.toString(), message);
+  }
+};
+
+handler.help = ['tiktoksearch *<txt>*'];
+handler.register = true;
+handler.tags = ['buscador'];
+handler.command = ['tiktoksearch', 'ttss', 'ttsearch'];
+
+export default handler;
