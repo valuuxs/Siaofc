96ac5c409dce331b0838439483c209c0528118e3
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-16 16:41:17 -0500

    Actualizar _test2.js

--- plugins/_test2.js
+++ plugins/_test2.js
@@ -1,50 +1,31 @@
-const handler = async (m, {conn, args, groupMetadata, participants, usedPrefix, command, isBotAdmin, isSuperAdmin}) => {
-  if (!args[0] || isNaN(args[0])) return m.reply(`*🍭 Ingrese Algún Prefijo De Un País: ${usedPrefix + command} 52*`);
+const handler = async (m, { conn, args, participants, usedPrefix, command }) => {
+  if (!args[0] || isNaN(args[0])) 
+    return m.reply(`*[ ℹ️ ] Ingrese algún prefijo de un país.\n\n*[ 💡 ] ${usedPrefix + command} 54*`);
 
-  const lol = args[0].replace(/[+]/g, '');
-  const pesan = args.slice(1).join(' ') || '📢 ¡Atención!';
-  const colombia = `🎩 *Mensaje:* ${pesan}`;
-  const ps = participants.map((u) => u.id).filter((v) => v !== conn.user.jid && v.startsWith(lol));
+  const prefijo = args[0].replace(/[+]/g, '');
+  const mensaje = args.slice(1).join(' ') || '📢 ¡Atención!';
+  const textoMensaje = `☕ *Mensaje:* ${mensaje}`;
 
-  if (ps.length === 0) return m.reply(`*🍭 No Hay Ningún Número Con El Prefijo +${lol} En Este Grupo.*`);
+  const usuariosConPrefijo = participants
+    .map((u) => u.id)
+    .filter((v) => v !== conn.user.jid && v.startsWith(prefijo));
 
-  const numeros = ps.map((v) => '┋💙 @' + v.replace(/@.+/, ''));
-  const delay = (time) => new Promise((res) => setTimeout(res, time));
+  if (usuariosConPrefijo.length === 0) 
+    return m.reply(`*[ ⚠️ ] No hay ningún número con el prefijo \`+${prefijo}\` en este grupo.*`);
 
-  switch (command) {
-    case 'hidnum':
-    case 'tagnum':
-      conn.reply(m.chat, `*☄️ MENSAJE ESPECIAL PARA +${lol} QUE ESTÁN EN ESTE GRUPO:*\n` + `${colombia}\n\n` + numeros.join`\n`, m, { mentions: ps });
-      break;
+  const listaUsuarios = usuariosConPrefijo.map((v) => '┋💙 @' + v.replace(/@.+/, '')).join('\n');
 
-    case 'removenum':
-      if (!isBotAdmin) return m.reply('*⚠️ El bot necesita permisos de administrador para eliminar usuarios.*');
-
-      const ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net';
-
-      for (const user of ps) {
-        if ([ownerGroup, conn.user.jid, global.owner + '@s.whatsapp.net'].includes(user) || isSuperAdmin) continue;
-
-        try {
-          await delay(2000);
-          const response = await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
-          if (response[0].status === '404') {
-            m.reply(`@${user.split('@')[0]} ya ha sido eliminado o ha abandonado el grupo.`, m.chat, { mentions: [user] });
-          }
-        } catch (e) {
-          m.reply(`*🚨 Error eliminando a @${user.split('@')[0]}*`, m.chat, { mentions: [user] });
-        }
-
-        await delay(10000);
-      }
-      break;
-  }
+  conn.reply(
+    m.chat, 
+    `*MENSAJE ESPECIAL PARA \`+${prefijo}\` QUE ESTÁN EN ESTE GRUPO:*\n\n${textoMensaje}\n\n${listaUsuarios}`, 
+    m, 
+    { mentions: usuariosConPrefijo }
+  );
 };
 
-handler.help = ['notifynum2 *<prefix>*', 'removenum *<prefix>*'];
-handler.command = /^(notifynum2|hidnum2|hidetagnum2|removenum2)$/i;
+handler.help = ['notifynum2 *<prefix>*'];
+handler.command = /^(notifynum2|nnum2|hidetagnum2)$/i;
 handler.group = true;
-handler.botAdmin = true;
 handler.admin = true;
 handler.fail = null;
 
