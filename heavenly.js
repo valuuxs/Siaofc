3b406ad8071c000ead855901e1936abeceb14647
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-16 19:45:59 -0500

    Actualizar heavenly.js

--- heavenly.js
+++ heavenly.js
@@ -303,7 +303,7 @@ console.log(chalk.bold.greenBright(`🤍 Todos los Sub-Bots se conectaron con é
 (async () => {
 global.conns = [];
 
-const mainBotAuthFile = 'ShadowwSession';
+const mainBotAuthFile = 'ShadowSession';
 try {
 const mainBot = await connectionUpdate(mainBotAuthFile);
 global.conns.push(mainBot);
