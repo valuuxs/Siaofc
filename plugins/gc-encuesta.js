aa31d90895481565a3874b4bd9cc630923a5123a
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-18 00:57:24 -0500

    Actualizar gc-encuesta.js

--- plugins/gc-encuesta.js
+++ plugins/gc-encuesta.js
@@ -1,23 +1,23 @@
-
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
-if (!args[0]) {
-conn.reply(m.chat, `⚠️️ *_Ingrese un texto para iniciar la escuesta._*\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`m)
-return
-}
-if (!text.includes('|')) {
-conn.reply(m.chat, `⚠️️ Separe las encuestas con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`m)
-}
-let a = []
-let b = text.split('|')
-for (let c = 0; c < b.length; c++) {
-a.push([b[c]])
-                        }
-                        return conn.sendPoll(m.chat, `${wm}`, a, m)
-}
-handler.help = ['encuesta *<text|text2>*']
-handler.tags = ['gc'] 
-handler.command = ['poll', 'encuesta'] 
-handler.group = true
+    if (!args[0]) {
+        conn.reply(m.chat, `⚠️️ *_Ingrese un texto para iniciar la encuesta._*\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`, m);
+        return;
+    }
 
-export default handler
+    if (!text.includes('|')) {
+        conn.reply(m.chat, `⚠️️ Separe las opciones de la encuesta con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`, m);
+        return;
+    }
+
+    let opciones = text.split('|').map(opcion => [opcion]); 
+
+    return conn.sendPoll(m.chat, `📊 Encuesta:`, opciones, m);
+};
+
+handler.help = ['encuesta *<texto|texto2>*'];
+handler.tags = ['gc'];
+handler.command = ['poll', 'encuesta'];
+handler.group = true;
+
+export default handler;
