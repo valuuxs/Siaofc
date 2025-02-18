1f963a6b844f7e5fa13684a59c5a3db50419b958
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-18 01:41:52 -0500

    Actualizar gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -1,4 +1,4 @@
-/*import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
+import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
 import * as fs from 'fs';
 const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
   try {
@@ -39,4 +39,4 @@ handler.command = /^(viso)$/i;
 handler.customPrefix = /a|A/i;
 handler.group = true;
 handler.admin = true;
-export default handler;*/
+export default handler;
