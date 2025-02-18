8ec2a229060284e9fd8ff430765e2f997c6e64bc
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-18 01:11:54 -0500

    Actualizar gc-encuesta.js

--- plugins/gc-encuesta.js
+++ plugins/gc-encuesta.js
@@ -1,23 +1,23 @@
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
     if (!args[0]) {
-        conn.reply(m.chat, `⚠️️ *_Ingrese un texto para iniciar la encuesta._*\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`, m);
+        conn.reply(m.chat, `*[ ℹ️ ] Ingrese un texto para iniciar la encuesta.*\n\n*[ 💡 ] Ejemplo:*\n${usedPrefix + command} *texto|texto2*`, m);
         return;
     }
 
     if (!text.includes('|')) {
-        conn.reply(m.chat, `⚠️️ Separe las opciones de la encuesta con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`, m);
+        conn.reply(m.chat, `*[ ⚠️ ] Separe las opciones de la encuesta con `\|\`\n\n*[ 💡 ] Ejemplo:*\n${usedPrefix + command} *texto|texto2*`, m);
         return;
     }
 
     let opciones = [...new Set(text.split('|'))].map(opcion => [opcion]); // Elimina duplicados
 
     if (opciones.length < 2) {
-        conn.reply(m.chat, `⚠️️ *_Debe haber al menos dos opciones diferentes en la encuesta._*`, m);
+        conn.reply(m.chat, `*[ ⚠️️ ] Debe haber al menos dos opciones diferentes en la encuesta.*`, m);
         return;
     }
 
-    return conn.sendPoll(m.chat, `📊 Encuesta:`, opciones, m);
+    return conn.sendPoll(m.chat, `*📊 Encuesta:*`, opciones, m);
 };
 
 handler.help = ['encuesta *<texto|texto2>*'];
