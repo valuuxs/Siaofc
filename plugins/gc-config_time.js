e95036e048d60c23cad69243271de37944f42f2f
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-17 20:31:58 -0500

    Actualizar plugins

---
+++ plugins/gc-config_time.js
@@ -0,0 +1,55 @@
+const handler = async (m, {conn, isAdmin, isOwner, args, usedPrefix, command}) => {
+  if (!(isAdmin || isOwner)) {
+          global.dfail('admin', m, conn);
+    throw false;
+  }
+  const isClose = {
+          'open': 'not_announcement',
+          'buka': 'not_announcement',
+    'on': 'not_announcement',
+          '1': 'not_announcement',
+          'close': 'announcement',
+          'tutup': 'announcement',
+    'off': 'announcement',
+    '0': 'announcement',
+  }[(args[0] || '')];
+  if (isClose === undefined) {
+          const caption = `
+*[ ℹ️ ] Coloca la opción y el número de horas lo cual se mantendrá el grupo abierto o cerrado.*
+
+*[ 💡 ] Ejemplo:*
+${usedPrefix + command} *open 1*
+${usedPrefix + command} *close 1*
+
+> ⍴ᥲrᥲ 𝗊ᥙᥱ ᥱᥣ grᥙ⍴᥆ ᥱs𝗍ᥱ *abierto/cerrado* ⍴᥆r ᥙᥒᥲ һ᥆rᥲ.
+`;
+    m.reply(caption);
+          throw false;
+  }
+  const timeoutset = 86400000 * args[1] / 24;
+  await conn.groupSettingUpdate(m.chat, isClose).then(async (_)=> {
+          m.reply(`*[ ⚠️ ] Grupo ${isClose == 'announcement' ? 'cerrado' : 'abierto'} ${args[1] ? `durante* \`\`\`${clockString(timeoutset)}\`\`\` *hora(s)*` : ''}`);
+  });
+  if (args[1]) {
+         setTimeout(async () => {
+      await conn.groupSettingUpdate(m.chat, `${isClose == 'announcement' ? 'not_announcement' : 'announcement'}`).then(async (_)=>{
+                    conn.reply(m.chat, `${isClose == 'not_announcement' ? '*[ ℹ️ ] El comando ah sido activado, es hora de cerrar.*' : '*[ ℹ️ ] La hora ah finalizado, el grupo será abierto.*'}!`);
+            });
+    }, timeoutset);
+  }
+};
+handler.help = ['grouptime *<open/close>* *<num>*'];
+handler.tags = ['gc'];
+handler.command = /^(grouptime|gctime)$/i;
+handler.botAdmin = true;
+handler.group = true;
+
+export default handler;
+
+function clockString(ms) {
+  const h = Math.floor(ms / 3600000);
+  const m = Math.floor(ms / 60000) % 60;
+  const s = Math.floor(ms / 1000) % 60;
+  console.log({ms, h, m, s});
+  return [h, m, s].map((v) => v.toString().padStart(2, 0) ).join(':');
+}
