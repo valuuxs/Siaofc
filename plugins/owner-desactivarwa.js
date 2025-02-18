c42dad68e714fdbe976ff01d8ccdad62d6694ff3
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-17 22:16:48 -0500

    Actualizar owner-desactivarwa.js

--- plugins/owner-desactivarwa.js
+++ plugins/owner-desactivarwa.js
@@ -1,15 +1,16 @@
 import cheerio from 'cheerio';
 import axios from 'axios';
 import util from 'util';
+
 const handler = async (m, {conn, isOwner, usedPrefix, command, args}) => {
   const q = args.join(' ');
-/*
-  if (!q || !args[0]) throw '*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙴𝙽 𝙵𝙾𝚁𝙼𝙰𝚃𝙾 𝙸𝙽𝚃𝙴𝚁𝙽𝙰𝙲𝙸𝙾𝙽𝙰𝙻, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: +𝟷 (𝟺𝟻0) 555-555*';
-*/
 
-if (!q || !args[0]) {
+  // Verificación de si no se pasa un número
+  if (!q || !args[0]) {
     return m.reply('*[ ℹ️ ] Ingrese el número que desee desactivar en formato internacional.*\n\n[ 💡 ] Ejemplo:* .soporte +1 (450) 555-555');
+  }
 
+  // Continuación del proceso si se pasó un número
   const ntah = await axios.get('https://www.whatsapp.com/contact/noclient/');
   const email = await axios.get('https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=10');
   const cookie = ntah.headers['set-cookie'].join('; ');
@@ -35,14 +36,9 @@ if (!q || !args[0]) {
   form.append('__ccg', 'UNKNOWN');
   form.append('__rev', '1006630858');
   form.append('__comment_req', '0');
+
   const res = await axios({url, method: 'POST', data: form, headers: {cookie}});
   const payload = String(res.data);
+
   if (payload.includes(`"payload":true`)) {
-    m.reply(`##- WhatsApp Support -##\n\nHola,\n\nGracias por tu mensaje.\n\nHemos desactivado tu cuenta de WhatsApp. Esto significa que su cuenta está deshabilitada temporalmente y se eliminará automáticamente en 30 días si no vuelve a registrar la cuenta. Tenga en cuenta: el equipo de atención al cliente de WhatsApp no puede eliminar su cuenta manualmente.\n\nDurante el período de cierre:\n • Es posible que sus contactos en WhatsApp aún vean su nombre y foto de perfil.\n • Cualquier mensaje que sus contactos puedan enviar a la cuenta permanecerá en estado pendiente por hasta 30 días.\n\nSi desea recuperar su cuenta, vuelva a registrar su cuenta lo antes posible.\nVuelva a registrar su cuenta ingresando el código de 6 dígitos, el código que recibe por SMS o llamada telefónica. Si te vuelves a registrar\n\nSi tiene alguna otra pregunta o inquietud, no dude en ponerse en contacto con nosotros. Estaremos encantados de ayudar!`);
-  } else if (payload.includes(`"payload":false`)) {
-    m.reply(`##- WhatsApp Support -##\n\nHola:\n\nGracias por tu mensaje.\n\nPara proceder con tu solicitud, necesitamos que verifiques que este número de teléfono te pertenece. Por favor, envíanos documentación que nos permita verificar que el número es de tu propiedad, como una copia de la factura telefónica o el contrato de servicio.\n\nPor favor, asegúrate de ingresar tu número de teléfono en formato internacional completo. Para obtener más información sobre el formato internacional, consulta este artículo.\n\nSi tienes alguna otra pregunta o inquietud, no dudes en contactarnos. Estaremos encantados de ayudarte.`);
-  } else m.reply(util.format(JSON.parse(res.data.replace('for (;;);', ''))));
-};
-handler.command = /^(supportwa|swa|soporte|support|desactivarwa|mandsupport)$/i;
-handler.rowner = true;
-export default handler;
+    m.reply(`##- WhatsApp Support -##\n\nHola,\n\nGracias por tu mensaje.\n\nHemos desactivado tu cuenta de WhatsApp. Esto significa que su cuenta está deshabilitada temporalmente y se eliminará automáticamente en 30 días si no vuelve a registrar la cuenta. Tenga en cuenta: el equipo de atención al cliente de WhatsApp no puede eliminar su cuenta manualmente.\n\nDurante el período de cierre:\n • Es posible que sus contactos en WhatsApp aún vean su nombre y foto de perfil.\n • Cualquier mensaje que sus contactos puedan enviar a la cuenta permanecerá en estado pendiente por hasta 30 días.\n\nSi desea recuperar su cuenta, vuelva a registrar su cuenta lo antes
