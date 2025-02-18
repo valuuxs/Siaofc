7db13c0cdd4d3c29ed2816fd7e0ccb2bf35389b4
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-18 01:42:54 -0500

    Actualizar gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -33,10 +33,9 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
     }
   }
 };
-handler.help = ['Aviso *<txt>*'];
+handler.help = ['notify *<txt>*'];
 handler.tags = ['gc'];
-handler.command = /^(viso)$/i;
-handler.customPrefix = /a|A/i;
+handler.command = /^(hidetag|notify|notificar|notifi|noti|n|hidet)$/i;
 handler.group = true;
 handler.admin = true;
 export default handler;
