d3aed713b7e052633fdef95a3b9a93d9e269ec59
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-19 14:41:25 -0500

    Actualizar a-tocar.js

--- plugins/a-tocar.js
+++ plugins/a-tocar.js
@@ -1,33 +1,29 @@
-const handler = async (m, { conn, text, command }) => {
+const handler = async (m, { conn, text }) => {
   if (!text) {
     return conn.reply(m.chat, '🔥 Por favor ingresa un texto.', m);
   }
 
   const body = `SELECCIONA UNA OPCIÓN`;
 
-  await conn.sendMessage(m.chat, {
+  const templateButtons = [
+    { index: 1, quickReplyButton: { displayText: 'Tocar1', id: '.tocar1' } },
+    { index: 2, quickReplyButton: { displayText: 'Tocar2', id: '.tocar2' } },
+  ];
+
+  const buttonMessage = {
     text: body,
     footer: 'Opciones disponibles',
-    buttons: [
-      {
-        buttonId: '.tocar1',
-        buttonText: { displayText: 'Tocar1' },
-        type: 1,
-      },
-      {
-        buttonId: '.tocar2',
-        buttonText: { displayText: 'Tocar2' },
-        type: 1,
-      },
-    ],
+    templateButtons: templateButtons,
     headerType: 1,
-  }, { quoted: m });
+  };
+
+  await conn.sendMessage(m.chat, buttonMessage);
 };
 
 handler.command = ['tocar'];
 export default handler;
 
-// Manejadores para los botones
+// Manejadores de respuestas a los botones
 const tocar1Handler = async (m, { conn }) => {
   await conn.reply(m.chat, 'Hola', m);
 };
