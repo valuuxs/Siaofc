f792d58a234ba2dd2efb4513f73ca88f56b2c0bc
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-19 03:11:36 -0500

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,20 +1,24 @@
 const handler = async (m, { conn }) => {
-  /*if (!db.data.chats[m.chat].audios) return;
+  // Verifica si los audios están activados en el chat
+  if (!db.data.chats[m.chat].audios) return;
   if (!db.data.settings[conn.user.jid].audios_bot && !m.isGroup) return;
-*/
-  const vn = 'https://files.catbox.moe/u9lir2.opus'; // Enlace de tu audio Opus
 
-  conn.sendPresenceUpdate('recording', m.chat);
-  conn.sendMessage(m.chat, {
-    audio: { url: vn },
+  const audioUrl = 'https://files.catbox.moe/u9lir2.opus'; // Enlace del audio
+
+  // Indica que el bot está "grabando"
+  await conn.sendPresenceUpdate('recording', m.chat);
+
+  // Envía el audio como nota de voz
+  await conn.sendMessage(m.chat, {
+    audio: { url: audioUrl },
     ptt: true, // Lo envía como nota de voz
     mimetype: 'audio/ogg; codecs=opus',
     fileName: 'audio.opus'
   }, { quoted: m });
 };
 
-// Comando que activará el audio
-handler.customPrefix = /^(a|ª|A)$/; 
-handler.command = new RegExp; 
+// Configurar el comando para que responda a "A", "a" o "ª"
+handler.customPrefix = /^(a|A|ª)$/i;
+handler.command = new RegExp(); // Permite usar solo el prefijo sin más texto
 
 export default handler;
