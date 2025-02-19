95341c7c130298dcf3e7b4ed0862b0faeccd460e
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-19 14:33:14 -0500

    Actualizar a-tocar.js

--- plugins/a-tocar.js
+++ plugins/a-tocar.js
@@ -1,20 +1,20 @@
-const handler = async (m, { conn, text, command }) => {
+const handler = async (m, { conn, text }) => {
     if (!text) {
         return conn.reply(m.chat, '⚠️ Agrega un texto después del comando.', m);
     }
 
-    const body = `✅ *SELECCIONA UNA OPCIÓN*`;
-
-    await conn.sendMessage(m.chat, {
-        text: body,
+    const message = {
+        text: "✅ *SELECCIONA UNA OPCIÓN*",
         footer: "Prueba de botones",
         buttons: [
-            { buttonId: `.tocar1`, buttonText: { displayText: '🎵 Tocar1' }, type: 1 },
-            { buttonId: `.tocar2`, buttonText: { displayText: '🎶 Tocar2' }, type: 1 },
+            { buttonId: "tocar1", buttonText: { displayText: "🎵 Tocar1" }, type: 1 },
+            { buttonId: "tocar2", buttonText: { displayText: "🎶 Tocar2" }, type: 1 },
         ],
         headerType: 1
-    }, { quoted: m });
+    };
+
+    await conn.sendMessage(m.chat, message, { quoted: m });
 };
 
-handler.command = ['tocar'];
+handler.command = ["tocar"];
 export default handler;
