a993114980f29f6c2009d0a485b7fc5da39063b2
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-18 23:25:03 -0500

    Actualizar rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -53,5 +53,5 @@ hours = (hours < 10) ? '0' + hours : hours
 minutes = (minutes < 10) ? '0' + minutes : minutes
 seconds = (seconds < 10) ? '0' + seconds : seconds
 
-return minutes + ' m y ' + seconds + ' s '
+return minutes + ' m y ' + seconds + ' s'
 }
