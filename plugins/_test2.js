727b1300398d17e150ddb7cd8c2f752657d55052
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-19 09:44:35 -0500

    Actualizar _test2.js

--- plugins/_test2.js
+++ plugins/_test2.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
 import fetch from 'node-fetch';
 const handler = async (m, {conn, command, usedPrefix}) => {
-  if (!db.data.chats[m.chat].nsfw && m.isGroup) throw '🚩 *¡Estos comandos están desactivados!*';
+  if (!db.data.chats[m.chat].nsfw && m.isGroup) {
+return m.reply('🚩 *¡Estos comandos están desactivados!*';
 
    switch (command) {
     case 'pack':
