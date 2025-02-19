dd50cfd7ac55eae7d83ba2d7e4aab8830d740daa
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-19 03:08:58 -0500

    Actualizar _test.js

---
+++ plugins/_test.js
@@ -0,0 +1,20 @@
+const handler = async (m, { conn }) => {
+  if (!db.data.chats[m.chat].audios) return;
+  if (!db.data.settings[conn.user.jid].audios_bot && !m.isGroup) return;
+
+  const vn = 'https://files.catbox.moe/u9lir2.opus'; // Enlace de tu audio Opus
+
+  conn.sendPresenceUpdate('recording', m.chat);
+  conn.sendMessage(m.chat, {
+    audio: { url: vn },
+    ptt: true, // Lo envía como nota de voz
+    mimetype: 'audio/ogg; codecs=opus',
+    fileName: 'audio.opus'
+  }, { quoted: m });
+};
+
+// Comando que activará el audio
+handler.customPrefix = /^(a|ª|A)$/; 
+handler.command = new RegExp; 
+
+export default handler;
