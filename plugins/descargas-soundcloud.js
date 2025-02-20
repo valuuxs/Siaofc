628ba40ee1f82d5ccb210cd04a147fcd1b6be114
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-20 00:38:03 -0500

    Actualizar descargas-soundcloud.js

--- plugins/descargas-soundcloud.js
+++ plugins/descargas-soundcloud.js
@@ -34,7 +34,7 @@ await m.react('❌');
 
 handler.help = ['play *<txt>*']
 handler.tags = ['descargas']
-handler.command = ['soundcloud', 'play']
+handler.command = ['soundcloud', 'splay']
 
 export default handler
 
