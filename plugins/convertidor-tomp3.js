19e03176d61ac45308edf70239d13468040adbb6
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-20 23:50:03 -0500

    Actualizar convertidor-tomp3.js

--- plugins/convertidor-tomp3.js
+++ plugins/convertidor-tomp3.js
@@ -22,6 +22,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
 };
 
 handler.help = ['tomp3', 'toaudio'];
+handler.tags = ['convertidor']
 handler.command = ['tomp3', 'toaudio'];
 handler.register = true;
 
