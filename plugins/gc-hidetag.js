071271111c236acfe17b9cc2eeaae1554fd7a4a1
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-20 22:21:55 -0500

    Actualizar gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -1,4 +1,4 @@
-import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
+/*import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
 import * as fs from 'fs';
 const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
   try {
@@ -38,4 +38,31 @@ handler.tags = ['gc'];
 handler.command = /^(hidetag|notify|notificar|notifi|noti|n|hidet)$/i;
 handler.group = true;
 handler.admin = true;
-export default handler;
+export default handler;*/
+
+import MessageType from '@whiskeysockets/baileys'
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
+
+let handler = async (m, { conn, text, participants }) => {
+let users = participants.map(u => conn.decodeJid(u.id))
+let q = m.quoted ? m.quoted : m
+let c = m.quoted ? m.quoted : m.msg
+const msg = conn.cMod(m.chat,
+generateWAMessageFromContent(m.chat, {
+[c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
+text: c || ''
+}
+}, {
+userJid: conn.user.id
+}),
+text || q.text, conn.user.jid, { mentions: users }
+)
+await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+}
+handler.help = ['notify <txt>']
+handler.tags = ['gc']
+handler.command = /^(hidetag|notify|notificar|notifi|noti|n|hidet)$/i;
+handler.group = true
+handler.admin = true
+
+export default handler
