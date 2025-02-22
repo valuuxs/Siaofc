08eba2e958d89c981f19dc61d4a3dcc4150ba0ac
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-21 22:10:06 -0500

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+/*import fetch from "node-fetch";
 import yts from 'yt-search';
 import axios from "axios";
 
@@ -160,3 +160,4 @@ function formatViews(views) {
     return views.toString();
   }
 }
+*/
