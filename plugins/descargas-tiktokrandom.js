ba891b73dd8dcfa925971bffbc5731215b528f8a
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-23 01:40:53 -0500

    Actualizar descargas-tiktokrandom.js

--- plugins/descargas-tiktokrandom.js
+++ plugins/descargas-tiktokrandom.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-/*const query = ['story%20wa','story%20sad','video%20fun','story%20wa%20galau','story%20wa%20sindiran','story%20wa%20bahagia','story%20wa%20lirik%20lagu%20overlay','story%20wa%20lirik%20lagu','video%20viral']*/
 const query = ['rolitas', 'rolitaschidas', 'estados', 'divertido', 'viral', 'musica', 'frases', 'automotivacion']
 
 let handler = async (m, {
