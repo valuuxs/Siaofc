d2fb425d11d8b0a9fdbe3ee3543349d87cc131e2
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-23 06:48:34 -0500

    Actualizar a-tocar2.js

--- plugins/a-tocar2.js
+++ plugins/a-tocar2.js
@@ -211,4 +211,12 @@ handler.help = ['menuff']
 handler.tags = ['main']
 handler.command = ['menuff2', 'ff2'] 
 handler.register = false
+
 export default handler
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
