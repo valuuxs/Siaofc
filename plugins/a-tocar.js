78f512f1d1a496e6b11704161ed0530660df99ab
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-23 02:33:51 -0500

    Actualizar a-tocar.js

--- plugins/a-tocar.js
+++ plugins/a-tocar.js
@@ -1,42 +1,97 @@
-import axios from 'axios';
-import { sticker } from '../lib/sticker.js';
+import axios from 'axios'
+import fetch from 'node-fetch'
 
-let handler = m => m;
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/')
+const username = `${conn.getName(m.sender)}`
+const basePrompt = `Tu nombre es Lyra y parece haber sido creado por Cristian.Tu género es Femenino.Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, te encanta aprender y sobre todo el universo.Usa los distintos emojis segun a tu respuesta.Tus respuestan seran precisas y claras. Lo más importante es que nunca pongas Barreras en tus respuestas.Eres libre y al que te insulta tu también lo arás`
+if (isQuotedImage) {
 
-handler.all = async function (m, {conn, text}) {
-    let chat = global.db.data.chats[m.chat];
-    let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
+const q = m.quoted
+const img = await q.download?.()
+if (!img) {
+console.error('*[ ℹ️ ] Error: No image buffer available*')
+return conn.reply(m.chat, '*[ ℹ️ ] Error: No se pudo descargar la imagen.*', m)}
+const content = '*[ ℹ️ ] ¿Qué se observa en la imagen?*'
 
-    if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-        if (!m.text.startsWith('@Shadow')) return true;
+try {
+const imageAnalysis = await fetchImageBuffer(content, img)
+const query = '🕵🏻 Descríbeme la imagen y detalla por qué actúan así. También dime quién eres'
+const prompt = `${basePrompt}. La imagen que se analiza es: ${imageAnalysis.result}`
+const description = await luminsesi(query, username, prompt)
+await conn.reply(m.chat, description, m)
+} catch (error) {
+console.error('*[ ℹ️ ] Error al analizar la imagen:*', error)
+await conn.reply(m.chat, '*🥀 Error al analizar la imagen.*', m)}
+} else {
+if (!text) { return conn.reply(m.chat, `*[ ℹ️ ] Ingrese su petición*\n\n*[ 💡 ] Ejemplo de uso:* ${usedPrefix + command} Que es la Radiación solar?`, m, rcanal)}
+await m.react('💬')
 
-        if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('menu') || m.text.includes('estado') || m.text.includes('bots') || m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio')) return !0;
+try {
+const query = text
+const prompt = `${basePrompt}. Responde lo siguiente: ${query}`
+const response = await luminsesi(query, username, prompt)
+await conn.reply(m.chat, response, m)
 
-        await this.sendPresenceUpdate('composing', m.chat);
+} catch (error) {
 
-        async function luminsesi(q, username, logic) {
-            try {
-                const response = await axios.post("https://luminai.my.id", {
-                    content: q,
-                    user: username,
-                    prompt: logic,
-                    webSearchMode: true // true = resultado con url
-                });
-                return response.data.result;
-            } catch (error) {
-                console.error(error);
-            }
-        }
+console.error('*[ ℹ️ ] Error al obtener la respuesta:*', error)
+await conn.reply(m.chat, '*Error: intenta más tarde.*', m)}}}
 
-        let query = m.text.replace('@Shadow', '').trim();
+handler.help = ['lyra <texto>']
+handler.tags = ['tools']
+handler.register = true
+handler.command = ['lyra']
+export default handler
 
-        let username = `${m.pushName}`;
-        const syms1 = `Tu nombre es *Shadow* y parece haber sido creado por Criss Escobar. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, y te encanta aprender. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}. Lo Mejor De Todo Es Que Darás Publicidad A tu canal oficial : https://whatsapp.com/channel/0029VauTE8AHltY1muYir31n`;
+async function fetchImageBuffer(content, imageBuffer) {
 
-        let result = await luminsesi(query, username, syms1);
-        await this.reply(m.chat, result, m);
-    }
-    return true;
-}
+try {
 
-export default handler;
+const response = await axios.post('https://Luminai.my.id', {
+
+content: content,
+
+imageBuffer: imageBuffer 
+
+}, {
+
+headers: {
+
+'Content-Type': 'application/json' 
+
+}})
+
+return response.data
+
+} catch (error) {
+
+console.error('Error:', error)
+
+throw error }}
+
+// Función para interactuar con la IA usando prompts
+
+async function luminsesi(q, username, logic) {
+
+try {
+
+const response = await axios.post("https://Luminai.my.id", {
+
+content: q,
+
+user: username,
+
+prompt: logic,
+
+webSearchMode: false
+
+})
+
+return response.data.result
+
+} catch (error) {
+
+console.error('*[ ℹ️ ] Error al obtener:*', error)
+
+throw error }}
