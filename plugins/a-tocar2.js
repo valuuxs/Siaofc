172a34b38ba1ee6e8133e5971df0501f22420482
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-23 06:56:20 -0500

    Actualizar a-tocar2.js

--- plugins/a-tocar2.js
+++ plugins/a-tocar2.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
-import moment from 'moment-timezone'
+import { xpRange } from '../lib/levelling.js'
 import { promises } from 'fs'
 import { join } from 'path'
 
