2e9ae1c62de2746f3c1464771006d327fd8accf1
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-24 01:44:07 -0500

    Actualizar buscador-xnxxsearch.js

--- plugins/buscador-xnxxsearch.js
+++ plugins/buscador-xnxxsearch.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     return m.reply('*[ ℹ️ ] El contenido `+18` está desactivado para este chat.*\n> ᥙsᥱ *enable nsfw* ⍴ᥲrᥲ ᥲᥴ𝗍і᥎ᥲrᥣ᥆.');
     }
 
-  if (!text) throw m.reply(`*[ 🔞 ] Ingresa el texto de lo que quieres buscar en Xnxx*\n\n*[ 💡 ] Ejemplo:* ${usedPrefix + command} Con mi Prima.`)
+  if (!text) throw m.reply(`*[ 🔎 ] Ingresa el texto de lo que quieres buscar en Xnxx*\n\n*[ 💡 ] Ejemplo:* ${usedPrefix + command} Con mi Prima.`)
   let response = await fetch(`https://api.agatz.xyz/api/xnxx?message=${text}`)
   let res = await response.json()
 
