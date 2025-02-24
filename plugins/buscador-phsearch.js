5274cae6ab88f68e00810100047768b958198125
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-24 01:43:49 -0500

    Actualizar buscador-phsearch.js

--- plugins/buscador-phsearch.js
+++ plugins/buscador-phsearch.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, args, command, usedPrefix }) => {
   } 
 
   if (!args[0]) {
-    return conn.reply(m.chat, `*[ 🔞 ] Por favor, ingrese la búsqueda que desea realizar en PornHub.*\n\n*[ 💡 ] Ejemplo:* ${usedPrefix + command} Gótica Culona.`, m);
+    return conn.reply(m.chat, `*[ 🔎 ] Por favor, ingrese la búsqueda que desea realizar en PornHub.*\n\n*[ 💡 ] Ejemplo:* ${usedPrefix + command} Gótica Culona.`, m);
   }
 
   try {
