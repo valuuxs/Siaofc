6377f52578f459a2474d94d73191e55c5dfe9764
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-24 01:43:20 -0500

    Actualizar descargas-pornhub.js

--- plugins/descargas-pornhub.js
+++ plugins/descargas-pornhub.js
@@ -19,7 +19,7 @@ https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
 import fetch from 'node-fetch'
 
 let HS = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, '[ 🔞 ] Ingresa un link de pornhub', m)
+if (!text) return conn.reply(m.chat, '[ 🔗 ] Ingresa un link de pornhub', m)
 
 //si borras creditos eri gei 👀
 
