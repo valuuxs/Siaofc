a3ae385e4e5ae51d424b98c82829fa4bb75e3117
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-24 01:40:10 -0500

    Actualizar descargas-pornhub.js

--- plugins/descargas-pornhub.js
+++ plugins/descargas-pornhub.js
@@ -19,8 +19,10 @@ https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
 import fetch from 'node-fetch'
 
 let HS = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, '❀ ingresa un link de pornhub', m)
+if (!text) return conn.reply(m.chat, '[ 🔞 ] Ingresa un link de pornhub', m)
+
 //si borras creditos eri gei 👀
+
 try {
 let api = await fetch(`https://www.dark-yasiya-api.site/download/phub?url=${text}`)
 let json = await api.json()
@@ -35,3 +37,4 @@ HS.command = ['pornhub', 'phdl']
 
 export default HS
 //Dejen creditos 👀 [ By Jtxs ] https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+//FAST DEJATE DE WBDS
