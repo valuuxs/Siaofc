324c8dce41603532cc5dde371819a78034f24938
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-24 01:53:36 -0500

    Actualizar descargas-pornhub.js

--- plugins/descargas-pornhub.js
+++ plugins/descargas-pornhub.js
@@ -19,6 +19,10 @@ https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
 import fetch from 'node-fetch'
 
 let HS = async (m, { conn, command, text, usedPrefix }) => {
+    if (!db.data.chats[m.chat].nsfw && m.isGroup) {
+    return m.reply('*[ ℹ️ ] El contenido `+18` está desactivado para este chat.*\n> ᥙsᥱ *enable nsfw* ⍴ᥲrᥲ ᥲᥴ𝗍і᥎ᥲrᥣ᥆.');
+    }
+
 if (!text) return conn.reply(m.chat, '[ 🔗 ] Ingresa un link de pornhub', m)
 
 //si borras creditos eri gei 👀
