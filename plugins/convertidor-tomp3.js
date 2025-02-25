75f5870ed98c732cea83e25cd4eb84a0aba1a334
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-25 14:34:04 -0500

    Actualizar convertidor-tomp3.js

--- plugins/convertidor-tomp3.js
+++ plugins/convertidor-tomp3.js
@@ -15,7 +15,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
 
   const audio = await toAudio(media, 'mp4');
   if (!audio.data) {
-    return conn.reply(m.chat, '*[ ❌ ] Ocurrio un error al convertir su nota de voz a Audio/MP3.*', m);
+    return conn.reply(m.chat, '*[ ❌ ] Ocurrio un error al convertir su nota de voz a audio.*', m);
   }
 
   conn.sendMessage(m.chat, {audio: audio.data, mimetype: 'audio/mpeg'}, {quoted: m});
@@ -23,7 +23,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
 
 handler.help = ['tomp3', 'toaudio'];
 handler.tags = ['convertidor']
-handler.command = ['tomp3', 'toaudio'];
+handler.command = ['tomp3', 'toaudio', 'toaud'];
 handler.register = true;
 
 export default handler;
