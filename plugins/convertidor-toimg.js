3e2ad9369dfdcd4fa8d315d02fdf2feb54061c13
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-25 22:54:45 -0500

    Actualizar convertidor-toimg.js

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -1,65 +1,24 @@
-import axios from 'axios';
-const { proto, generateWAMessageFromContent, generateWAMessageContent } = (await import("@whiskeysockets/baileys")).default;
+let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
-let handler = async (m, { conn, text }) => {
-    if (!text) return conn.reply(m.chat, '*¿Qué quieres buscar en TikTok?*', m);
+let handler = async (m, { conn }) => {
+if (!m.quoted) return conn.reply(m.chat, `🚩 Responde a una imagen ViewOnce.`, m)
+if (m.quoted.mtype !== 'viewOnceMessageV2') return conn.reply(m.chat, `🚩 Responde a una imagen ViewOnce.`, m)
+let msg = m.quoted.message
+let type = Object.keys(msg)[0]
+let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
+let buffer = Buffer.from([])
+for await (const chunk of media) {
+buffer = Buffer.concat([buffer, chunk])
+}
+if (/video/.test(type)) {
+return conn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m)
+} else if (/image/.test(type)) {
+return conn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
+}}
+handler.help = ['ver']
+handler.tags = ['tools']
+handler.command = ['readviewonce', 'read', 'ver', 'readvo'] 
+//handler.limit = 1
+handler.register = true 
 
-    async function createVideoMessage(url) {
-        const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer });
-        return videoMessage;
-    }
-
-    try {
-        await m.react('🕓');
-        conn.reply(m.chat, '*Descargando su video...*', m);
-
-        let apiUrl = `https://delirius-apiofc.vercel.app/search/tiktoksearch?query=${encodeURIComponent(text)}`;
-        let { data: response } = await axios.get(apiUrl);
-
-        if (!response || response.status !== 200 || !response.meta || response.meta.length === 0) {
-            return conn.reply(m.chat, '*No se encontraron resultados para tu búsqueda.*', m);
-        }
-
-        let results = await Promise.all(response.meta.map(async result => ({
-            body: proto.Message.InteractiveMessage.Body.fromObject({ text: null }),
-            footer: proto.Message.InteractiveMessage.Footer.fromObject({ text: '🔎 TikTok - Búsquedas' }),
-            header: proto.Message.InteractiveMessage.Header.fromObject({
-                title: result.title || "Sin título",
-                hasMediaAttachment: true,
-                videoMessage: await createVideoMessage(result.hd) // URL del video sin marca de agua
-            }),
-            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] })
-        })));
-
-        const responseMessage = generateWAMessageFromContent(m.chat, {
-            viewOnceMessage: {
-                message: {
-                    messageContextInfo: {
-                        deviceListMetadata: {},
-                        deviceListMetadataVersion: 2
-                    },
-                    interactiveMessage: proto.Message.InteractiveMessage.fromObject({
-                        body: proto.Message.InteractiveMessage.Body.create({ text: `Resultados de: ${text}` }),
-                        footer: proto.Message.InteractiveMessage.Footer.create({ text: '🔎 TikTok - Búsquedas' }),
-                        header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
-                        carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: results })
-                    })
-                }
-            }
-        }, { quoted: m });
-
-        await m.react('✅');
-        await conn.relayMessage(m.chat, responseMessage.message, { messageId: responseMessage.key.id });
-
-    } catch (error) {
-        console.error('Error en la búsqueda de TikTok:', error);
-        await conn.reply(m.chat, '⚠️ Ocurrió un error al buscar en TikTok.', m);
-    }
-};
-
-handler.help = ['tiktoksearch <texto>'];
-handler.tags = ['search'];
-handler.command = ['tiz'];
-handler.register = true;
-//handler.corazones = 1;
-export default handler;
+export default handler
