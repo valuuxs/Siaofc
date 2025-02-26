0e51258e79f29f1bb4782cf282c79e36951d3b20
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-26 10:40:19 -0500

    Actualizar plugins

---
+++ plugins/herramientas-readViewOnce.js
@@ -0,0 +1,17 @@
+let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
+
+let handler = async (m, { conn }) => {
+if (!m.quoted) return conn.reply(m.chat, `*[ ℹ️ ] Responde a una imagen ViewOnce (ver solo vez)*.`, m)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `*[ ℹ️ ] Responde a una imagen ViewOnce (ver solo una vez)*`, m)
+let buffer = await m.quoted.download(false);
+if (/videoMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
+} else if (/imageMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m)
+}}
+handler.help = ['read']
+handler.tags = ['herramientas']
+handler.command = ['readviewonce', 'read', 'readvo', 'ver'] 
+handler.register = true 
+
+export default handler
