5bf6636a1ad5c012a5434b2a03a426127b6f2ed8
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-27 12:16:04 -0500

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -11,7 +11,10 @@ const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     return total;
   }, 0);
 
-  conn.reply(m.chat, `*[ 🔎 ] ¡El poder está en tus manos!*\n*Bienvenido ${taguser}*\n\n*Este bot cuenta con \`${totalf}\` comandos disponibles para ti. ¿Tienes alguna sugerencia para mejorar nuestra experiencia? ¡No dudes en contactar con mi creador!*\n\n> © Տһᥲძᨣᥕ Ɓᨣƚ Uᥣ𝗍rᥲ`, m);
+  conn.sendMessage(m.chat, {
+text: `*[ 🔎 ] ¡El poder está en tus manos!*\n*Bienvenido ${taguser}*\n\n*Este bot cuenta con \`${totalf}\` comandos disponibles para ti. ¿Tienes alguna sugerencia para mejorar nuestra experiencia? ¡No dudes en contactar con mi creador!*\n\n> © Տһᥲძᨣᥕ Ɓᨣƚ Uᥣ𝗍rᥲ`,
+mentions: [m.sender]
+}, { quoted: fkontak });
 };
 handler.command = ['totalcomandos', 'comandostotales', 'totalf'];
 export default handler;
