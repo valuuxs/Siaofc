e9d7b8056847d144b2f1356b8c381e6f4ab25d9a
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-27 18:37:49 -0500

    Actualizar ztest-buttons.js

--- plugins/ztest-buttons.js
+++ plugins/ztest-buttons.js
@@ -1,1 +1,49 @@
-//
+/*
+//Buttons - Message Im Interactive
+
+let handler = async (m, { conn }) => {
+    let imageUrl = 'https://files.catbox.moe/ilr818.jpg';
+
+    conn.sendMessage(m.chat, { 
+        image: { url: imageUrl }, 
+        caption: 'TEXTO 🍒', 
+        footer: dev, 
+        buttons: [
+            {
+                buttonId: `.owner`,
+                buttonText: { displayText: 'owner' }
+            }
+        ],
+        viewOnce: true,
+        headerType: 4
+    }, { quoted: m });
+}
+
+handler.tag = ['info'];
+handler.help = ['p'];
+handler.command = ['p'];
+
+export default handler;
+
+// Buttons Ix
+
+let handler = async (m, { conn }) => {
+
+    conn.sendMessage(m.chat, { 
+        text: 'TITLE', 
+        footer: dev, 
+        buttons: [
+            {
+                buttonId: `.owner`,
+                buttonText: { displayText: 'Owner' },
+                type: 1
+            }
+        ],
+        viewOnce: true
+    }, { quoted: m });
+}
+
+handler.tag = ['info'];
+handler.help = ['p'];
+handler.command = ['p'];
+*/
