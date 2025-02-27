e0a0cb5bd5cbec0d4bee2fcdd6cb8483c86f4348
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-27 12:04:39 -0500

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,6 +1,5 @@
 const handler = async (m, { conn }) => {
-  const name = conn.getName(m.sender);
-  const usertag = '@' + m.sender.split('@s.whatsapp.net')[0];
+const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
   let totalf = Object.values(global.plugins).reduce((total, plugin) => {
     if (plugin.command) {
       if (Array.isArray(plugin.command)) {
