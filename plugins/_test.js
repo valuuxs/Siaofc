a39bdc38b2ceb55ae276c68c5a2c50fb69fd3e3e
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-27 10:53:29 -0500

    Actualizar _test.js

---
+++ plugins/_test.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn }) => {
+  let totalf = Object.values(global.plugins).reduce((total, plugin) => {
+    if (plugin.command) {
+      if (Array.isArray(plugin.command)) {
+        return total + plugin.command.length;
+      } else {
+        return total + 1;
+      }
+    }
+    return total;
+  }, 0);
+
+  conn.reply(m.chat, `🤖 El bot tiene *${totalf}* comandos disponibles.`, m);
+};
+handler.command = ['totalcomandos', 'comandostotales'];
+export default handler;
