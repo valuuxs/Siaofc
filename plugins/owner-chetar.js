8738a7b9623c4177061f603c21e2aaf0a94e0ed3
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-28 00:37:48 -0500

    Actualizar plugins

---
+++ plugins/owner-chetar.js
@@ -0,0 +1,15 @@
+
+const handler = async (m, { conn }) => {
+    const user = global.db.data.users[m.sender];
+        conn.sendMessage(m.chat, {text: `*[ ℹ️ ] @${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: fkontak});
+      global.db.data.users[m.sender].money = Infinity;
+    global.db.data.users[m.sender].estrellas = Infinity;
+  global.db.data.users[m.sender].level = Infinity;
+ global.db.data.users[m.sender].exp = Infinity;
+};
+handler.help = ['cheat'];
+handler.tags = ['owner'];
+handler.command = /^(ilimitado|infiniy|chetar)$/i;
+handler.mods = true;
+handler.fail = null;
+export default handler;
