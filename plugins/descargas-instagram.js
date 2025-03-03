d4bc95de2a4a301f9c4c44ac3482055647781da0
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-03 09:37:00 -0500

    Actualizar plugins

---
+++ plugins/descargas-instagram.js
@@ -0,0 +1,27 @@
+import { igdl } from "ruhend-scraper"
+
+let handler = async (m, { args, conn }) => {
+  if (!args[0]) {
+    return conn.reply(m.chat, '*[ ☕ ] Ingresa un link de Instagram*')
+  }
+  try {
+    await m.react('⏳️')
+    conn.reply(m.chat, `*[ ☕ ] Ƈᴀʀɢᴀɴᴅᴏ...*\n▰▰▰▰▰▰▰▰▭▭`)
+    let res = await igdl(args[0])
+    let data = res.data
+    for (let media of data) {
+      await new Promise(resolve => setTimeout(resolve, 2000))
+      await conn.sendFile(m.chat, media.url, 'instagram.mp4', '*_DESCARGAS - INSTAGRAM_*\n\n> * [ 🍢 ] Vídeo de Instagram descargado correctamente por Shadow Bot - MD*')
+    }
+  } catch {
+    await m.react('❌')
+    conn.reply(m.chat, '*[ ℹ️ ] Ocurrió un error.*')
+  }
+}
+
+handler.command = ['instagram', 'ig', 'instagram2', 'ig2']
+handler.tags = ['downloader']
+handler.help = ['instagram', 'ig']
+handler.register = true
+
+export default handler
