a2c9fb92964a7604d3f4efebc83fe7c6acf705d3
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-06 10:58:33 -0500

    Actualizar config-nable.js

--- plugins/config-nable.js
+++ plugins/config-nable.js
@@ -373,6 +373,21 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
     chat.nsfw = isEnable          
     break
 
+  case 'audios':
+    case 'audiosbot':
+    case 'botaudios':
+      if (!m.isGroup) {
+        if (!isOwner) {
+          global.dfail('group', m, conn)
+          throw false
+        }
+      } else if (!isAdmin) {
+        global.dfail('admin', m, conn)
+        throw false
+      }
+      chat.audios = isEnable
+      break
+
  case 'modoadmin':
     case 'soloadmin':
       if (m.isGroup) {
