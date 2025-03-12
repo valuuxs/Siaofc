03b660d1d0509b35cd1df778e60ead41b56dc91c
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-11 19:08:05 -0500

    Actualizar nsfw-videoxxx.js

--- plugins/nsfw-videoxxx.js
+++ plugins/nsfw-videoxxx.js
@@ -1,4 +1,3 @@
-// Para los pajeros xd
 let handler = async(m, { conn }) => {
 
 let chat = global.db.data.chats[m.chat];
@@ -6,15 +5,13 @@ if (!chat.nsfw) return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨�
 
 let rvid = global.vidxxx[Math.floor(Math.random() * global.vidxxx.length)];
 
-//let vid = 'https://dark-core-api.vercel.app/api/random/anime-random-hot?key=dk-vip';
-
 conn.sendMessage(m.chat, { 
         video: { url: rvid }, 
-        caption: '🍭 ¡Disfruta Del Video!', 
+        caption: '🍒 ¡Disfruta Del Video!', 
         footer: dev, 
         buttons: [
             {
-                buttonId: `.vxxx4`,
+                buttonId: `.vxxx`,
                 buttonText: { displayText: 'Siguiente Vídeo' }
             }
         ],
@@ -25,26 +22,13 @@ conn.sendMessage(m.chat, {
 
 handler.tag = ['emox'];
 handler.help = ['videoxxx'];
-handler.command = ['videoxxx4', 'vxxx4'];
+handler.command = ['videoxxx', 'vxxx'];
 
 export default handler;
 
 global.vidxxx = [
-    'https://telegra.ph/file/4a270d9945ac46f42d95c.mp4',
-    'https://telegra.ph/file/958c11e84d271e783ea3f.mp4',
-    'https://telegra.ph/file/f753759342337c4012b3f.mp4',
-    'https://telegra.ph/file/379cee56c908dd536dd33.mp4',
-    'https://telegra.ph/file/411d8f59a5cefc2a1d227.mp4',
-    'https://telegra.ph/file/ee2cf1b359d6eef50d7b7.mp4',
-    'https://telegra.ph/file/1e316b25c787f94a0f8fd.mp4',
-    'https://telegra.ph/file/c229d33edce798cde0ca4.mp4',
-    'https://telegra.ph/file/b44223e72dd7e80e415f2.mp4',
-    'https://telegra.ph/file/61486d45a8a3ea95a7c86.mp4',
-    'https://telegra.ph/file/76ba0dc2a07f491756377.mp4',
-    'https://telegra.ph/file/831bb88f562bef3f1a15d.mp4',
-    'https://telegra.ph/file/ee2cf1b359d6eef50d7b7.mp4',
-    'https://telegra.ph/file/598857924f3a29ffd37ae.mp4',
-    'https://telegra.ph/file/528caef6ea950ec45aeef.mp4',
+    'https://telegra.ph/file/4a270d9945ac46f42d95c.mp4', 'https://telegra.ph/file/958c11e84d271e783ea3f.mp4', 'https://telegra.ph/file/f753759342337c4012b3f.mp4', 'https://telegra.ph/file/379cee56c908dd536dd33.mp4', 'https://telegra.ph/file/411d8f59a5cefc2a1d227.mp4', 'https://telegra.ph/file/ee2cf1b359d6eef50d7b7.mp4', 'https://telegra.ph/file/1e316b25c787f94a0f8fd.mp4', 'https://telegra.ph/file/c229d33edce798cde0ca4.mp4', 'https://telegra.ph/file/b44223e72dd7e80e415f2.mp4', 'https://telegra.ph/file/61486d45a8a3ea95a7c86.mp4', 'https://telegra.ph/file/76ba0dc2a07f491756377.mp4', 'https://telegra.ph/file/831bb88f562bef3f1a15d.mp4', 'https://telegra.ph/file/ee2cf1b359d6eef50d7b7.mp4', 'https://telegra.ph/file/598857924f3a29ffd37ae.mp4',
+'https://telegra.ph/file/528caef6ea950ec45aeef.mp4',
 'https://files.catbox.moe/nm0hdi.mp4',
 'https://files.catbox.moe/prdlor.mp4',
 'https://files.catbox.moe/58lkvi.mp4',
