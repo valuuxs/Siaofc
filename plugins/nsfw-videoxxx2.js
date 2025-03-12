80ada7e7a9a2a13483368095494cdbf492d04f3f
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-11 19:32:37 -0500

    Actualizar plugins

---
+++ plugins/nsfw-videoxxx2.js
@@ -0,0 +1,27 @@
+let handler = async(m, { conn }) => {
+
+let chat = global.db.data.chats[m.chat];
+if (!chat.nsfw) return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
+
+let vid = 'https://dark-core-api.vercel.app/api/random/anime-random-hot?key=dk-vip';
+
+conn.sendMessage(m.chat, { 
+        video: { url: vid }, 
+        caption: '🍒 ¡Disfruta Del Video!', 
+        footer: dev, 
+        buttons: [
+            {
+                buttonId: `.vxxx2`,
+                buttonText: { displayText: 'Siguiente Vídeo' }
+            }
+        ],
+        viewOnce: true,
+        headerType: 4
+    }, { quoted: m });
+}
+
+handler.tag = ['nsfw'];
+handler.help = ['videoxxx2'];
+handler.command = ['videoxxx2', 'vxxx2'];
+
+export default handler;
