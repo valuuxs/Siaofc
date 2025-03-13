4f8fef428c8561283f50cd61d73ce4baad9d32c5
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-13 13:36:12 -0500

    Actualizar _test4.js

--- plugins/_test4.js
+++ plugins/_test4.js
@@ -39,13 +39,13 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
                                         header: video.title,
                                         title: video.author.name,
                                         description: 'Descargar MP3',
-                                        id: `${prefijo}ytmp3 ${video.url}`
+                                        id: `.ytmp3 ${video.url}`
                                     },
                                     {
                                         header: video.title,
                                         title: video.author.name,
                                         description: 'Descargar MP4',
-                                        id: `${prefijo}ytmp4 ${video.url}`
+                                        id: `.ytmp4 ${video.url}`
                                     }
                                 ]
                             }))
