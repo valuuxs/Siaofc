053970cf1e521503aefeccd9152d103a10b7c04f
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-13 00:01:48 -0500

    Actualizar ztest-buttons.js

--- plugins/ztest-buttons.js
+++ plugins/ztest-buttons.js
@@ -145,7 +145,7 @@ var handler = async (m, { text, conn }) => {
 
 handler.help = ['ytsearch']
 handler.tags = ['buscador']
-handler.command = ['youtubesearch', 'ytsearch', 'yts']
+handler.command = ['xds']
 handler.register = true
 
 export default handler;
