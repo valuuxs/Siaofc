554be5cac53a07aad89c782f00a2e1db2728efe6
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-14 18:24:25 -0500

    Actualizar herramientas-traductor.js

--- plugins/herramientas-traductor.js
+++ plugins/herramientas-traductor.js
@@ -1,4 +1,4 @@
-const { prepareWAMessageMedia, generateWAMessageFromContent } = (await import('@whiskeysockets/baileys')).default;
+/*const { prepareWAMessageMedia, generateWAMessageFromContent } = (await import('@whiskeysockets/baileys')).default;
 // const { prepareWAMessageMedia, generateWAMessageFromContent } = await import("@whiskeysockets/baileys");
 const { randomBytes } = await import("crypto");
 
@@ -84,4 +84,26 @@ const handler = async (m, { conn }) => {
 
 handler.command = ["tes"];
 
-export default handler;
+export default handler;*/
+
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+    if (!text) return conn.reply(m.chat, `${emoji} Ingrese un texto para hablar con Llama AI.`, m)
+    try {
+        let api = await fetch(`https://delirius-apiofc.vercel.app/ia/llamaia?query=${text}`)
+        let json = await api.json()
+        let responseMessage = json.data;
+
+        await conn.sendMessage(m.chat, {
+            text: responseMessage
+        }, { quoted: m });
+
+    } catch (error) { 
+        console.error(error)
+    }
+}
+
+handler.command = ['llama', 'meta']
+
+export default handler
