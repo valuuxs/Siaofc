acbf9afe4aa6395af73bfb5e52a7d80516f26ffe
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-14 10:35:31 -0500

    Actualizar info-sugerencia.js

--- plugins/info-sugerencia.js
+++ plugins/info-sugerencia.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text }) => {
-    const suittag = '51927238856'; // Reemplaza con el número del propietario sin el + ni espacios
+    const suittag = '51927238856'; // Número del propietario con código de país, sin espacios
     const user = m.pushName || 'Usuario'; // Nombre del usuario o por defecto "Usuario"
 
     if (!text) {
-        return conn.reply(m.chat, '*[ ℹ️ ] ¡Hola ${user}! ¿Qué comando quieres sugerir?*', m);
+        return conn.reply(m.chat, `*[ ℹ️ ] ¡Hola ${user}! ¿Qué comando quieres sugerir?*`, m);
     }
     if (text.length < 10) {
         return conn.reply(m.chat, '*[ ⚠️ ] La sugerencia debe tener al menos 10 caracteres.*', m);
@@ -12,7 +12,7 @@ let handler = async (m, { conn, text }) => {
         return conn.reply(m.chat, '*[ ⚠️ ] El máximo permitido para la sugerencia es de 1000 caracteres.*', m);
     }
 
-    const teks = `*[ 💡 ] Sugerencia de nuevo comando de ${nombre}:*
+    const teks = `*[ 💡 ] Sugerencia de nuevo comando de ${user}:*
 
 *📝 Comando sugerido:*
 > ${text}`;
