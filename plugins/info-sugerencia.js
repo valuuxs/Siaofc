9d9583dcd7c453134edaa26bac2844a25fa4e1ac
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-14 10:33:49 -0500

    Actualizar info-sugerencia.js

--- plugins/info-sugerencia.js
+++ plugins/info-sugerencia.js
@@ -1,20 +1,20 @@
 let handler = async (m, { conn, text }) => {
     const suittag = '51927238856'; // Reemplaza con el número del propietario sin el + ni espacios
-    const nombre = m.pushName || 'Usuario'; // Nombre del usuario o por defecto "Usuario"
+    const user = m.pushName || 'Usuario'; // Nombre del usuario o por defecto "Usuario"
 
     if (!text) {
-        return conn.reply(m.chat, '¿Qué comando quieres sugerir?', m);
+        return conn.reply(m.chat, '*[ ℹ️ ] ¡Hola ${user}! ¿Qué comando quieres sugerir?*', m);
     }
     if (text.length < 10) {
-        return conn.reply(m.chat, 'La sugerencia debe tener al menos 10 caracteres.', m);
+        return conn.reply(m.chat, '*[ ⚠️ ] La sugerencia debe tener al menos 10 caracteres.*', m);
     }
     if (text.length > 1000) {
-        return conn.reply(m.chat, 'El máximo permitido para la sugerencia es de 1000 caracteres.', m);
+        return conn.reply(m.chat, '*[ ⚠️ ] El máximo permitido para la sugerencia es de 1000 caracteres.*', m);
     }
 
-    const teks = `Sugerencia de nuevo comando de ${nombre}:
+    const teks = `*[ 💡 ] Sugerencia de nuevo comando de ${nombre}:*
 
-Comando sugerido:
+*📝 Comando sugerido:*
 > ${text}`;
 
     // Enviar sugerencia al propietario
@@ -23,7 +23,7 @@ Comando sugerido:
     });
 
     // Confirmación al usuario
-    m.reply('Tu sugerencia ha sido enviada al propietario.');
+    m.reply('*[ ✅ ] ¡Tu sugerencia ha sido enviada al propietario!* Gracias por tu aporte.');
 };
 
 handler.help = ['newcommand <texto>'];
