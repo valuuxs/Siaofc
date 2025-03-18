5b8b1619f73e4fcb5dc4932e4493011b51825fe5
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-18 14:30:52 -0500

    Actualizar buscador-lyrics.js

---
+++ plugins/buscador-lyrics.js
@@ -0,0 +1,37 @@
+/* Lyrics By WillZek 
+- Free Codes Titan 
+- https://github.com/WillZek
+- https://whatsapp.com/channel/0029ValMlRS6buMFL9d0iQ0S 
+*/
+
+import fetch from 'node-fetch';
+
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+
+if (!text) return m.reply(m.chat, '*[ 🔍 ] Ingrese el nombre de una canción para buscar la letra.*', m);
+
+try {
+let api = `https://archive-ui.tanakadomp.biz.id/search/lirik?q=${text}`;
+
+let responde = await fetch(api);
+let json = await responde.json();
+let crow = json.result;
+
+let txt = `*Nombre:* ${crow.title}\n*Letra:* ${crow.lyrics}`;
+
+let img = crow.thumb;
+
+conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
+
+} catch (e) {
+console.log(e)
+m.reply('*[ ❌ ] No se pudo obtener la letra De su canción*');
+m.reply('❌');
+ }
+};
+
+handler.help = ['lyrics'];
+handler.tag = ['buscador'];
+handler.command = ['letra', 'lyric', 'lyrics', 'lirik'];
+
+export default handler;
