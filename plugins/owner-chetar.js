f8a3b15842d2f4abb5a4ca23a4ad57e4f338e2ef
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-18 16:56:55 -0500

    Actualizar owner-chetar.js

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -3,7 +3,7 @@ const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
         conn.sendMessage(m.chat, {text: `*[ ℹ️ ] @${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: fkontak});
       global.db.data.users[m.sender].money = Infinity;
-    global.db.data.users[m.sender].estrellas = Infinity;
+    global.db.data.users[m.sender].diamantes = Infinity;
   global.db.data.users[m.sender].level = Infinity;
  global.db.data.users[m.sender].exp = Infinity;
 };
