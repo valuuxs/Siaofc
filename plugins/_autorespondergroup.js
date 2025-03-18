788060e49abc56783a5c9dd3de5c5f8a62f1108f
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-18 01:24:52 -0500

    Actualizar _autorespondergroup.js

--- plugins/_autorespondergroup.js
+++ plugins/_autorespondergroup.js
@@ -7,9 +7,7 @@ if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
 conn.reply(m.chat, `🌠 ¡Hola! Soy Shadow, en que puedo ayudarte hoy?\n\n✰ Usa *.menu* para ver mis comandos.`, m, rcanal, )
 }
-/*if (/^que|q$/i.test(m.text)) {
-conn.reply(m.chat, `*so*`, m, rcanal, )
-}*/
+
 if (/^English$/i.test(m.text)) {
 conn.reply(m.chat, `*The first one to speak is gay*`, m, rcanal, )
 }
