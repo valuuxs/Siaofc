c9723e9dfea5b45362448d44491e953fcab47679
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-18 23:19:42 -0500

    Actualizar herramientas-readViewOnce.js

--- plugins/herramientas-readViewOnce.js
+++ plugins/herramientas-readViewOnce.js
@@ -1,4 +1,4 @@
-let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
+/*let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
 if (!m.quoted) return conn.reply(m.chat, `*[ ℹ️ ] Responde a una imagen ViewOnce (ver solo vez)*.`, m)
@@ -14,4 +14,30 @@ handler.tags = ['herramientas']
 handler.command = ['readviewonce', 'read', 'readvo', 'ver'] 
 handler.register = true 
 
-export default handler
+export default handler*/
+
+//Mejorado por Criss 
+
+let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
+
+let handler = async (m, { conn }) => {
+    if (!m.quoted) return conn.reply(m.chat, `*[ ℹ️ ] Responde a un mensaje ViewOnce (ver solo una vez)*.`, m);
+    if (!m.quoted.viewOnce) return conn.reply(m.chat, `*[ ℹ️ ] Responde a un mensaje ViewOnce (ver solo una vez)*`, m);
+    
+    let buffer = await m.quoted.download(false);
+
+    if (/videoMessage/.test(m.quoted.mtype)) {
+        return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m);
+    } else if (/imageMessage/.test(m.quoted.mtype)) {
+        return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted.caption || '', m);
+    } else if (/audioMessage/.test(m.quoted.mtype)) {
+        return conn.sendFile(m.chat, buffer, 'audio.mp3', '', m, { mimetype: 'audio/mp3' });
+    }
+};
+
+handler.help = ['read']
+handler.tags = ['herramientas']
+handler.command = ['readviewonce', 'read', 'readvo', 'ver'] 
+handler.register = true 
+
+export default handler;
