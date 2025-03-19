f21c8a2f04cc937f41b0887d782d98a8eb94cda4
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-19 17:03:17 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -43,6 +43,5 @@ const handler = async (m, { text, conn, args, usedPrefix, command }) => {
 handler.help = ['facebook'];
 handler.tags = ['descargas']
 handler.command = /^(fb|facebook|fbdl)$/i;
-handler.register = true;
 
 export default handler;                                                                                                                                                                                                                              
