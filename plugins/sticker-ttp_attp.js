f1bec0add0d3578629f042bd4afd6bcd5c29ac9b
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-20 16:56:28 -0500

    Actualizar sticker-ttp_attp.js

--- plugins/sticker-ttp_attp.js
+++ plugins/sticker-ttp_attp.js
@@ -1,28 +1,20 @@
 import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch'
-import axios from 'axios'
+import axios from 'axios';
 
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-    if (!text) return m.reply(`❀ Por Favor, Ingresa Un Texto Para Realizar Tu Sticker.`)
-
-    let userId = m.sender
-    let packstickers = global.db.data.users[userId] || {}
-    let texto1 = packstickers.text1 || global.packsticker
-    let texto2 = packstickers.text2 || global.packsticker2
+let handler = async(m, { conn, text, args, usedPrefix, command }) => {
+    if (!text) return m.reply(`🌿 Ingresa Un Texto Para Realizar Tu Sticker\n> *Ejemplo:* ${usedPrefix + command} Shadow`)
 
     if (command == 'attp') {
-        let stiker = await sticker(null, `https://api.fgmods.xyz/api/maker/attp?text=${text}&apikey=elrebelde21`, texto1, texto2)
-        conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true)
-    }
+let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/attp?text=${text}&apikey=elrebelde21`,global.packname, global.author)
+conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: channel, thumbnail: imagen1 }}}, { quoted: m })}
 
     if (command == 'ttp') {
-        let stiker = await sticker(null, `https://api.fgmods.xyz/api/maker/ttp?text=${text}&apikey=elrebelde21`, texto1, texto2)
-        conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true)
-    }
+let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/ttp?text=${text}&apikey=elrebelde21`,global.packname, global.author)
+conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: redes, thumbnail: imagen1 }}}, { quoted: fkontak })
+}
 }
 
+handler.command = handler.help = ['ttp', 'attp']
 handler.tags = ['sticker']
-handler.help = ['ttp', 'attp']
-handler.command = ['ttp', 'attp']
-
 export default handler
