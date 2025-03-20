84c617067dbdd31eb3f9418c9b81e94d2123ba02
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-20 15:37:43 -0500

    Actualizar plugins

---
+++ plugins/inteligencia-lucia.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+  const username = `${conn.getName(m.sender)}`
+
+const basePrompt = `Serás Lucía`
+
+  if (!text) { 
+    return conn.reply(m.chat, `*[ ℹ️ ] Ingrese un texto para discutir con simi.*`, m)
+  }
+
+  await conn.sendPresenceUpdate('composing', m.chat) // Indica que está escribiendo
+
+  try {
+    const query = text
+    const prompt = `${basePrompt}. Responde lo siguiente: ${query}`
+    const response = await luminsesi(query, username, prompt)
+    await conn.reply(m.chat, response, m)
+  } catch (error) {
+    console.error('*[ ℹ️ ] Error al obtener la respuesta:*', error)
+    await conn.reply(m.chat, '*Error: intenta más tarde.*', m)
+  }
+}
+
+handler.help = ['lu']
+handler.tags = ['inteligencia']
+handler.command = ['lu', 'lucia']
+export default handler
+
+async function luminsesi(q, username, logic) {
+  try {
+    const response = await axios.post("https://Luminai.my.id", {
+      content: q,
+      user: username,
+      prompt: logic,
+      webSearchMode: false
+    })
+    return response.data.result
+  } catch (error) {
+    console.error('*[ ℹ️ ] Error al obtener:*', error)
+    throw error
+  }
+}
