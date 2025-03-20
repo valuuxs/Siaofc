08a5f9b4b902aa9eb18c934941a1edda8d1623ba
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-20 18:01:47 -0500

    Actualizar sticker-ttp_attp.js

--- plugins/sticker-ttp_attp.js
+++ plugins/sticker-ttp_attp.js
@@ -1,27 +1,28 @@
-
-import { sticker } from '../lib/sticker.js'
-import fetch from 'node-fetch'
-import axios from 'axios'
-
-let handler = async (m, { conn, text, command }) => {
-    if (!text) return m.reply(`*[ ℹ️ ] Ingresa un texto para realizar tu Sticker.*`)
-
-    let texto1 = global.packsticker
-    let texto2 = global.authsticker
-
-    if (command === 'attp') {
-        let stiker = await sticker(null, `https://api.fgmods.xyz/api/maker/attp?text=${encodeURIComponent(text)}&apikey=elrebelde21`, texto1, texto2)
-        conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true)
-    }
-
-    if (command === 'ttp') {
-        let stiker = await sticker(null, `https://api.fgmods.xyz/api/maker/ttp?text=${encodeURIComponent(text)}&apikey=elrebelde21`, texto1, texto2)
-        conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true)
-    }
-}
-
-handler.tags = ['sticker']
-handler.help = ['ttp', 'attp']
-handler.command = ['ttp', 'attp']
-
+import { sticker } from '../lib/sticker.js'  
+import fetch from 'node-fetch'  
+import axios from 'axios'  
+  
+let handler = async (m, { conn, text, command, fkontak }) => {  
+    if (!text) return m.reply(`*[ ℹ️ ] Ingresa un texto para realizar tu Sticker.*`)  
+  
+    let texto1 = global.packsticker  
+    let texto2 = global.authsticker  
+  
+    let url = ''  
+    if (command === 'attp') {  
+        url = `https://api.fgmods.xyz/api/maker/attp?text=${encodeURIComponent(text)}&apikey=elrebelde21`  
+    } else if (command === 'ttp') {  
+        url = `https://api.fgmods.xyz/api/maker/ttp?text=${encodeURIComponent(text)}&apikey=elrebelde21`  
+    }  
+  
+    if (url) {  
+        let stiker = await sticker(null, url, texto1, texto2)  
+        await conn.sendFile(m.chat, stiker, 'sticker.webp', '', fkontak, true) // Enviar con fkontak  
+    }  
+}  
+  
+handler.tags = ['sticker']  
+handler.help = ['ttp', 'attp']  
+handler.command = ['ttp', 'attp']  
+  
 export default handler
