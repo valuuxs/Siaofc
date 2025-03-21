acb6820ccbf9127982fd66174ea3949b79f5d9c1
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-21 17:32:52 -0500

    Actualizar gc-kick2.js

--- plugins/gc-kick2.js
+++ plugins/gc-kick2.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
+/*let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
   if (!global.db.data.settings[conn.user.jid].restrict)
 {
 return m.reply('*[ ⚠️ ] 𝙴𝙻 𝙾𝚆𝙽𝙴𝚁 𝚃𝙸𝙴𝙽𝙴 𝚁𝙴𝚂𝚃𝚁𝙸𝙽𝙶𝙸𝙳𝙾 (𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝 / 𝚍𝚒𝚜𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙴𝙻 𝚄𝚂𝙾 𝙳𝙴 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾*');
@@ -28,5 +28,39 @@ handler.customPrefix = /k|K|b|B|s|S|e|E|r|R/i;
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
+
+export default handler;*/
+
+// ❀ Código By JTxs
+
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+
+if (!text) return conn.reply(m.chat, '❀ Ingresa un link de Pornhub Para Descargar Su Video 🍭', m)
+try {
+let api = `https://www.dark-yasiya-api.site/download/phub?url=${text}`
+let res = await fetch(api);
+let json = await res.json()
+let resu = json.result.format[0];
+
+let { video_title, video_uploader } = json.result
+let { resolution, } = json.result.format[1]
+
+let url = resu.download_url;
+
+m.react('🕑');
+await conn.sendMessage(m.chat, { video: { url: url }, caption: video_title }, { quoted: m });
+m.react('✅');
+
+} catch (error) {
+m.reply(`Error: ${error.message}`);
+console.error(error)
+}}
+
+handler.command = ['pornhubdl', 'phdl'];
+handler.tag = ['descargas'];
+handler.help = ['pornohubdl'];
+//handler.estrellas = 9;
 
 export default handler;
