4088b54b7bfe07c3abf39e66966d702d22d115c8
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-29 22:44:12 -0500

    Actualizar _test4.js

--- plugins/_test4.js
+++ plugins/_test4.js
@@ -112,7 +112,7 @@ const handler = async (m, { conn, isPrems }) => {
 
 *𓈒𓏸🌴 \`Bot Name:\`* Morchi Bot - MD
 *𓈒𓏸🌵 \`Activo:\`* ${uptime}
-*𓈒𓏸🍃 \`Usuarios:\`* ${totalreg}
+*𓈒𓏸🍃 \`Usuarios:\`* 
 *𓈒𓏸🌿 \`Version:\`* 1.0.0
 
 *✪ \`Platform:\`* Linux
