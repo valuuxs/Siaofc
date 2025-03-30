cbbd231fca25483a643c9a235124b5df5d0398a1
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-29 22:39:45 -0500

    Actualizar _test3.js

--- plugins/_test3.js
+++ plugins/_test3.js
@@ -1,4 +1,4 @@
-/*import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
 const handler = async (m, {conn, usedPrefix, text, isPrems}) => {
 
@@ -77,4 +77,4 @@ function clockString(ms) {
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
   return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
-}*/
+}
