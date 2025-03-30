452dcd914e963d12dde201b568f398b0e0f6cf7d
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-03-29 23:45:21 -0500

    Actualizar _test4.js

--- plugins/_test4.js
+++ plugins/_test4.js
@@ -29,7 +29,7 @@ const handler = async (m, { conn, usedPrefix, isPrems }) => {
 ✪ Platform: Linux
 ✪ Baileys: Multi-Device
 ✪ Prefix [ . ]
-
+${readmore}
 > 😸 Si encuentra un comando con errores no dudes en reportarlo con el Creador
 `.trim();
 
@@ -60,6 +60,8 @@ handler.fail = null;
 
 export default handler;
 
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
 function clockString(ms) {
   const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
