4ee58f44cc50103ed2a74ebae93109eb655a99a1
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-05 18:22:50 -0500

    Actualizar descargas-aptoide.js

--- plugins/descargas-aptoide.js
+++ plugins/descargas-aptoide.js
@@ -39,6 +39,6 @@ const handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
   }
 };
 
-handler.command = /^(apk|apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
+handler.command = /^(apkmod|modapk|aptoide|aptoidedl)$/i;
 
 export default handler;
