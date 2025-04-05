4a12ff04d1104dcff1d863ef864b5772ab40432f
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-04 19:32:13 -0500

    Actualizar buscador-spotifysearch.js

--- plugins/buscador-spotifysearch.js
+++ plugins/buscador-spotifysearch.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 const { generateWAMessageContent, generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply('*[ ℹ️ ] Ingresa el texto de lo que quieres buscar en Spotify*\n\n*[ 💡 ] Ejemplo:* .spotifysearch Gata Only');
+if (!text) return m.reply('*🌴 Ingresa el texto de lo que quieres buscar en Spotify*\n> *\`Ejemplo:\`* .spotifysearch Gata Only');
 await m.react('🕓');
 
 try {
