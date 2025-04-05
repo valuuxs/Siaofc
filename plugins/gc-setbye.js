c86939c7f194f93bbf2ebb424ea675f1a740a77f
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-05 14:05:56 -0500

    Actualizar plugins

---
+++ plugins/gc-setbye.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
+
+if (text) {
+global.db.data.chats[m.chat].sBye = text
+conn.reply(m.chat, `*✅ La Despedida del grupo ha sido configurada*`, fkontak, m)  
+
+} else {
+    conn.reply(m.chat, `*🌵 ¡Escribe el mensaje de despedida!*\n✎ *Puedes usar:*\n\n- *\`@user\`* (Mención al Usuario)\n- *\`@group\`* (Nombre del Grupo)\n- *\`@desc\`* (Descripción del Grupo)\n\n> ☕ Los @ son opcionales`, fkontak, m)
+}
+}
+
+handler.help = ['setbye @user + texto']
+handler.tags = ['group']
+handler.command = ['setbye', 'despedida'] 
+handler.botAdmin = true
+handler.admin = true
+handler.group = true
+export default handler
