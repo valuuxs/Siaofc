fa2f877c57964d303f24b4141888dd1889f82e9c
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-05 00:03:19 -0500

    Actualizar buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -11,7 +11,7 @@ let response = await fetch(api);
 let json = await response.json();
 let result = json.results[0];
 
-let txt = `\`\`\`乂 GITHUB - SEARCH\`\`\`\n\n° 🌴 *\`Nombre:\`* ${result.name}\n° 🍀 *\`Dueño:\`* ${result.creator}\n° 🌵 *\`Estrellas:\`* ${result.stars}\n° 🌿 *\`Bifurcaciones:\`* ${result.forks}\n° 🌳 *\`Descripción:\`*\n${result.description}\n° 🍃 *\`Creado:\`* ${result.createdAt}\n° 🪨 *\`Link:*\` ${result.cloneUrl}`;
+let txt = `\`\`\`乂 GITHUB - SEARCH\`\`\`\n\n° 🌴 *\`Nombre:\`* ${result.name}\n° 🍀 *\`Dueño:\`* ${result.creator}\n° 🌵 *\`Estrellas:\`* ${result.stars}\n° 🌿 *\`Bifurcaciones:\`* ${result.forks}\n° 🌳 *\`Descripción:\`*\n${result.description}\n° 🍃 *\`Creado:\`* ${result.createdAt}\n° 🪨 *\`Link:\`* ${result.cloneUrl}`;
 
 let img = 'https://files.catbox.moe/9vlgt5.jpg';
 conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
