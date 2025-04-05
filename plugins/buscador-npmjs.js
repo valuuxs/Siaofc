19ed5fdb48a617904e38e504482cd003e1c4ca69
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-05 00:49:34 -0500

    Actualizar buscador-npmjs.js

--- plugins/buscador-npmjs.js
+++ plugins/buscador-npmjs.js
@@ -1,58 +1,35 @@
-/*
-《✧》DERECHOS RESERVADOS POR EL AUTOR《✧》
-- GabrielVz (@glytglobal)
-*/
-
 import fetch from 'node-fetch'
 
-let handler = async (m, { text, usedPrefix, command }) => {
+let handler = async (m, { text, usedPrefix, command, conn }) => {
 
-if (!text) return conn.reply(m.chat, `*🔎 Escribe el nombre del scraper.*\n> *\`Ejemplo:\`* ${usedPrefix + command} yt-search`, m)
+  if (!text) return conn.reply(m.chat, `*🔎 Escribe el nombre del scraper.*\n> *\`Ejemplo:\`* ${usedPrefix + command} yt-search`, m)
 
-try {
+  try {
+    await m.react('⏳')
 
-await m.react(rwait)
-/*conn.reply(m.chat, '*🔎 Buscando el scraper...*', m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons, 
-sourceUrl: channel }}})*/
+    let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${text}`)
+    let { objects } = await res.json()
 
-let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${text}`)
-let { objects } = await res.json()
+    if (!objects.length) return conn.reply(m.chat, `\`\`\`⚠️ No se encontraron resultados\`\`\``, m)
 
-if (!objects.length) return conn.reply(m.chat, `\`\`\`⚠️ No se enconraron resultados\`\`\``, m)
+    let txt = `\`\`\`乂 SCRAPER - SEARCH\`\`\``
 
-
-let txt = `\`\`\`乂 SCRAPER - SEARCH\`\`\``;
-json.data.forEach((app, index) => {
+    objects.forEach(({ package: pkg }, index) => {
       txt += `\n\n*\`${index + 1}\`*`
       txt += `\n≡ 🌴 *\`Nombre:\`* ${pkg.name}`
       txt += `\n≡ 🌵 *\`Versión:\`* V${pkg.version}`
-      txt += `\n≡ 🍃 *\`Description:\`* ${pkg.description}`
+      txt += `\n≡ 🍃 *\`Descripción:\`* ${pkg.description}`
       txt += `\n≡ 🌿 *\`Link:\`* ${pkg.links.npm}`
-}) 
+    })
 
-m.reply(txt)
+    await conn.reply(m.chat, txt, m)
+    await m.react('✅')
 
-/*
-let txt = objects.map(({ package: pkg }) => {
-return `\`\`\`乂 SCRAPER - SEARCH\`\`\`
-
-≡ 🍄 *\`Nombre:\`* ${pkg.name}
-≡ 🌹 *\`Versión:\`* V${pkg.version}
-≡ 🍁 *\`Link:\`* ${pkg.links.npm}
-≡ 🌷 *\`Descripción:\`* ${pkg.description}
-\n\n`
-}).join`\n\n`*/
-
-await conn.reply(m.chat, txt, m)*/
-await m.react('✅')
-} catch {
-await conn.reply(m.chat, '\`\`\`⚠️ Ocurrió un error\`\`\`', m)
-await m.react('❌')
-}}
+  } catch {
+    await conn.reply(m.chat, '```⚠️ Ocurrió un error```', m)
+    await m.react('❌')
+  }
+}
 
 handler.help = ['npmjs']
 handler.tags = ['buscador']
