a4b5e60cf80594e71f7d825bdcba4ab37305c0ca
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-04 22:39:33 -0500

    Actualizar buscador-yts2.js

--- plugins/buscador-yts2.js
+++ plugins/buscador-yts2.js
@@ -1,22 +1,30 @@
 import yts from 'yt-search'
-let handler = async (m, {conn, text }) => {
-  if (!text) throw `*🔎 ¿Que deseas buscar en Youtube?*`
+
+let handler = async (m, { conn, text }) => {
+  if (!text) throw `*🔎 ¿Qué deseas buscar en YouTube?*`
+
   let results = await yts(text)
-let tes = results.videos
-let ms = tes.map(v => `
+  let tes = results.videos
+
+  if (!tes.length) throw '\`\`\`⚠️ No se encontraron resultados.\`\`\`'
+
+  let ms = tes.map(v => `
 ° ${v.title}
 
-≡ 🌳 \`${mssg.duration} :\` ${v.timestamp}
-≡ 🌴 \`${mssg.aploud} :\` ${v.ago}
-≡ 🍁 \`${mssg.views} :\` ${v.views.toLocaleString()}
-≡ 🌿 \`${mssg.link} :\` ${v.url}
+≡ 🌳 *Duración:* ${v.timestamp}
+≡ 🌴 *Publicado:* ${v.ago}
+≡ 🍁 *Vistas:* ${v.views.toLocaleString()}
+≡ 🌿 *Enlace:* ${v.url}
 `.trim()).join('\n________________________\n\n')
-let teks = `\`\`\`乂 YOUTUBE - SEARCH\`\`\`\n\n${ms}`
-teks += `\n\n${footer}`
-        conn.sendFile(m.chat, tes[0].image, 'yts.jpeg', teks, m)
+
+  let teks = `\`\`\`乂 YOUTUBE - SEARCH\`\`\`\n\n${ms}`
+  teks += `\n\n> sʜᴀᴅᴏᴡ ᴜʟᴛʀᴀ ᴍᴅ`
+
+  conn.sendFile(m.chat, tes[0].image, 'yts.jpeg', teks, m)
 }
+
 handler.help = ['ytsearch'] 
 handler.tags = ['buscador']
-handler.command = ['ytsearch2', 'yts2'] 
+handler.command = ['ytsearch2', 'yts2']
 
 export default handler
