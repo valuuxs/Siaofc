c6781e513c3fd211b2f90b34bb32a3e824eda055
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-05 00:09:29 -0500

    Actualizar buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -13,10 +13,10 @@ let result = json.results[0];
 
 let txt = `\`\`\`乂 GITHUB - SEARCH\`\`\`\n\n° 🌴 *\`Nombre:\`* ${result.name}\n° 🍀 *\`Dueño:\`* ${result.creator}\n° 🌵 *\`Estrellas:\`* ${result.stars}\n° 🌿 *\`Bifurcaciones:\`* ${result.forks}\n° 🌳 *\`Descripción:\`*\n> ${result.description}\n° 🍃 *\`Creado:\`* ${result.createdAt}\n° 🪨 *\`Link:\`* ${result.cloneUrl}`;
 
-let img = 'https://files.catbox.moe/9vlgt5.jpg';
-conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
+//let img = 'https://files.catbox.moe/9vlgt5.jpg';
+//conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
 
-//conn.sendMessage(m.chat, caption: txt }, { quoted: fkontak });
+conn.sendMessage(m.chat, { text: txt }, { quoted: fkontak });
 
 } catch (error) {
 console.error(error)
