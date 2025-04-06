e6e954930a6d0ecd8d225df51469bf74e9d850ba
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-06 09:34:20 -0500

    Actualizar buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -13,7 +13,7 @@ let handler = async (message, { conn, text, usedPrefix, command }) => {
   try {
     // Mensaje inicial informando al usuario que se está descargando el video
     //conn.reply(message.chat, wait, message);
-      await m.react('⏳');
+      await message.react('⏳');
 
     // Realizar la búsqueda de TikTok
     let { data: response } = await axios.get('https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=' + text);
