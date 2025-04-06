f59720be5bf81087f49087f3b0f0fd65189b21c0
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-06 09:00:55 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -2,7 +2,7 @@
 
 const handler = async (m, { text, conn, args, usedPrefix, command }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, '*[ 🔗 ] Ingresa el link del video de Facebook.*\n\n*[ 💡 ] Ejemplo:* .fb https://www.facebook.com/reel/858448809516815/?mibextid=9drbnH&s=yWDuG2&fs=e', fkontak, m);
+    return conn.reply(m.chat, '*🥞 Por favor, ingresa un link de Facebook.*', fkontak, m);
   }
 
   await m.react('🕒');
@@ -10,23 +10,23 @@ const handler = async (m, { text, conn, args, usedPrefix, command }) => {
   try {
     res = await igdl(args[0]);
   } catch (error) {
-    return conn.reply(m.chat, '*[ ❌ ] Error al obtener el video, verifique que el enlace sea correcto*', m);
+    return conn.reply(m.chat, '*❌ Error al obtener el video, verifique que el enlace sea correcto*', m);
   }
 
   let result = res.data;
   if (!result || result.length === 0) {
-    return conn.reply(m.chat, '*[ ⚠️ ] No se encontraron resultados.*', m);
+    return conn.reply(m.chat, '*⚠️ No se encontraron resultados.*', m);
   }
 
   let data;
   try {
     data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
   } catch (error) {
-    return conn.reply(m.chat, '*[ ❌ ] Error al enviar el video de Facebook*', m);
+    return conn.reply(m.chat, '*❌ Error al enviar el video de Facebook*', m);
   }
 
   if (!data) {
-    return conn.reply(m.chat, '*[ ⚠️ ] No se encontró una resolución adecuada.*', m);
+    return conn.reply(m.chat, '*⚠️ No se encontró una resolución adecuada.*', m);
   }
 
   await m.react('✅');
@@ -35,7 +35,7 @@ const handler = async (m, { text, conn, args, usedPrefix, command }) => {
   try {
     await conn.sendMessage(m.chat, { video: { url: video }, caption: '> © ᥲ𝗊ᥙі 𝗍іᥱᥒᥱ sᥙ ᥎іძᥱ᥆ ძᥱ 𝖿ᥲᥴᥱᑲ᥆᥆k', fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: fkontak });
   } catch (error) {
-    return conn.reply(m.chat, '*[ ⚠️ ] La URL del vídeo está corrompida por lo cual no fue posible enviar el vídeo.*', m);
+    return conn.reply(m.chat, '*⚠️ La URL está corrupta, intenta con otra URL.*', m);
   await m.react('❌');
   }
 };
