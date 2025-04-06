a006c3120c374887652e8e4b6700ab76b01623b0
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-06 08:21:11 -0500

    Actualizar descargas-capcut.js

--- plugins/descargas-capcut.js
+++ plugins/descargas-capcut.js
@@ -19,7 +19,7 @@ const handler = async (m, { conn, usedPrefix, command, text }) => {
             return m.reply('*❌ No se pudieron obtener los datos. Asegúrate de que la URL ingresada sea correcta.*');
         }
 
-        const cpt = `*\`CAPCUT - DESCARGAS\`*\n\n> *• Título:* ${result.title}\n> *• Fecha:* ${result.date}\n> *• Usuario:* ${result.pengguna}\n> *• Me gusta:* ${result.likes}\n> *• Autor:* ${result.author.name}`;
+        const cpt = `\`\`\`◜CapCut - Download◞\`\`\`\n\n° *🌴 Titulo:* ${result.title}\n° *📆 Fecha:* ${result.date}\n° *👤 Autor:* ${result.author.name}\n° *🤍 Likes:* ${result.likes}\n\n> ${dev}`;
         await conn.sendFile(m.chat, result.videoUrl, '', cpt, m, {
             thumbnail: await fetch(result.posterUrl).then(res => res.buffer())
         });
