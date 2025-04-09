602dde2383156275be5c3e3d6efbf010302b96cd
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-09 17:01:11 -0500

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, text, usedPrefix, command, args }) => {
     let limit = 10485760;
     let size = await getSize(json.url);
 
-    const cap = `\`\`\`◜YouTube - MP4◞\`\`\`\n\n*${json.title}*\n≡ 🌴 \`URL\` : ${args[0]}\n≡ ⚖️ Peso: ${await formatSize(size) || "Desconocido"}`;
+    const cap = `\`\`\`◜YouTube - MP4◞\`\`\`\n\n*${json.title}*\n≡ 🌴 \`URL:\` ${args[0]}\n≡ ⚖️ \`Peso:\` ${await formatSize(size) || "Desconocido"}`;
 
     conn.sendFile(m.chat, await (await fetch(json.url)).buffer(), `${json.title}.mp4`, cap, m, null, { asDocument: true, mimetype: "video/mp4" })
 
