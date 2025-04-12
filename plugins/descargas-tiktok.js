fd1aa6cc16e59af6b71cf0aa23df8d1a08ee0e7a
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-12 11:23:20 -0500

    Actualizar descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) {
-        throw m.reply(`*🥞 Por favor, ingresa un link de Instagram.*`);
+        throw m.reply(`*🥞 Por favor, ingresa un link de TikTok.*`);
     }
 
     try {
