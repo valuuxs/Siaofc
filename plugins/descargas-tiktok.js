d3a8fbda0cf7880f1a6914ab0a374170511aeb8c
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-12 10:39:23 -0500

    Actualizar descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -1,4 +1,4 @@
-/*import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 
 var handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) {
@@ -46,54 +46,4 @@ async function tiktokdl(url) {
     let tikwm = `https://www.tikwm.com/api/?url=${url}?hd=1`
     let response = await (await fetch(tikwm)).json()
     return response
-}*/
-
-import fetch from 'node-fetch';
-
-var handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) {
-        throw m.reply(`*🥞 Por favor, ingresa un link de TikTok.*`);
-    }
-
-    if (!args[0].match(/(https?:\/\/)?(www\.)?(vm\.|vt\.)?tiktok\.com\//)) {
-        throw m.reply(`*⚠️ El link ingresado no es válido. Asegúrese de que sea un link de TikTok.*`);
-    }
-
-    try {
-        await m.react('⏳'); // Reacción de espera
-
-        const tiktokData = await tiktokdl(args[0]);
-
-        if (!tiktokData) {
-            //await m.react('❌');
-            throw m.reply("*❌ Error de la api*");
-        }
-
-        const videoURL = tiktokData.data.play;
-        const videoURLWatermark = tiktokData.data.wmplay;
-        const infonya_gan = `*📖 Descrip꯭ción:*\n> ${tiktokData.data.title}`;
-
-        if (videoURL || videoURLWatermark) {
-            await conn.sendFile(m.chat, videoURL, "tiktok.mp4", "\`\`\`◜TikTok - Download◞\`\`\`" + `\n\n${infonya_gan}`, m);
-            //await m.react('✅');
-        } else {
-            //await m.react('❌');
-            throw m.reply("*❌ No se pudo descargar.*");
-        }
-    } catch (error1) {
-        //await m.react('❌');
-        conn.reply(m.chat, `Error: ${error1}`, m);
-    }
-};
-
-handler.help = ['tiktok'];
-handler.tags = ['descargas'];
-handler.command = /^(tt|tiktok)$/i;
-
-export default handler;
-
-async function tiktokdl(url) {
-    let tikwm = `https://www.tikwm.com/api/?url=${url}?hd=1`;
-    let response = await (await fetch(tikwm)).json();
-    return response;
 }
