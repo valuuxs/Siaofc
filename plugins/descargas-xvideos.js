5809fd094aa5a92335bab3aa6bc7cbdf983128f4
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-12 21:59:41 -0500

    Actualizar descargas-xvideos.js

--- plugins/descargas-xvideos.js
+++ plugins/descargas-xvideos.js
@@ -2,50 +2,51 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import cheerio from 'cheerio';
 
-
-const handler = async (m, {conn, args, command, usedPrefix, text }) => {
-  const datas = global
-if (!db.data.chats[m.chat].nsfw && m.isGroup) {
+const handler = async (m, { conn, args, command, usedPrefix, text }) => {
+  if (m.isGroup && !(db.data.chats[m.chat] && db.data.chats[m.chat].nsfw)) {
     return m.reply('*🌿 El contenido* `+18` *está desactivado para este chat.*\n> Use *enable nsfw* para activarlo.');
-    }
+  }
+
   if (!args[0]) {
-  return conn.reply(m.chat, `*🫓 Por favor, ingresa un enlace válido de xvideos.*`, m);
-}
+    return conn.reply(m.chat, `*🫓 Por favor, ingresa un enlace válido de xvideos.*`, m);
+  }
 
   try {
     await m.react('⏳');
     const res = await xvideosdl(args[0]);
     conn.sendMessage(m.chat, { document: { url: res.result.url }, mimetype: 'video/mp4', fileName: res.result.title }, { quoted: m });
   } catch (e) {
-    throw `*⚠️ Proporciona un enace válido de xvideos.*`;
+    throw '*⚠️ Proporciona un enlace válido de xvideos.*';
   }
 };
+
 handler.command = /^(xvideosdl|xvdl|xvideos)$/i;
 export default handler;
 
 async function xvideosdl(url) {
   return new Promise((resolve, reject) => {
-    fetch(`${url}`, { method: 'get' })
+    fetch(url, { method: 'get' })
       .then(res => res.text())
       .then(res => {
         let $ = cheerio.load(res, { xmlMode: false });
-        const title = $("meta[property='og:title']").attr("content")
-        const keyword = $("meta[name='keywords']").attr("content")
-        const views = $("div#video-tabs > div > div > div > div > strong.mobile-hide").text() + " views"
-        const vote = $("div.rate-infos > span.rating-total-txt").text()
-        const likes = $("span.rating-good-nbr").text()
-        const deslikes = $("span.rating-bad-nbr").text()
-        const thumb = $("meta[property='og:image']").attr("content")
-        const url = $("#html5video > #html5video_base > div > a").attr("href")
-        resolve({ status: 200, result: { title, url, keyword, views, vote, likes, deslikes, thumb } })
+        const title = $("meta[property='og:title']").attr("content");
+        const keyword = $("meta[name='keywords']").attr("content");
+        const views = $("div#video-tabs > div > div > div > div > strong.mobile-hide").text() + " views";
+        const vote = $("div.rate-infos > span.rating-total-txt").text();
+        const likes = $("span.rating-good-nbr").text();
+        const deslikes = $("span.rating-bad-nbr").text();
+        const thumb = $("meta[property='og:image']").attr("content");
+        const videoUrl = $("#html5video > #html5video_base > div > a").attr("href");
+        resolve({ status: 200, result: { title, url: videoUrl, keyword, views, vote, likes, deslikes, thumb } });
       })
-  })
-};
+      .catch(err => reject(err));
+  });
+}
 
-async function xvideosSearch(url) {
+async function xvideosSearch(query) {
   return new Promise(async (resolve) => {
-    await axios.request(`https://www.xvideos.com/?k=${url}&p=${Math.floor(Math.random() * 9) + 1}`, { method: "get" }).then(async result => {
-      let $ = cheerio.load(result.data, { xmlMod3: false });
+    await axios.get(`https://www.xvideos.com/?k=${query}&p=${Math.floor(Math.random() * 9) + 1}`).then(async result => {
+      let $ = cheerio.load(result.data, { xmlMode: false });
       let title = [];
       let duration = [];
       let quality = [];
@@ -53,17 +54,20 @@ async function xvideosSearch(url) {
       let thumb = [];
       let hasil = [];
 
-      $("div.mozaique > div > div.thumb-under > p.title").each(function (a, b) {
+      $("div.mozaique > div > div.thumb-under > p.title").each(function () {
         title.push($(this).find("a").attr("title"));
         duration.push($(this).find("span.duration").text());
         url.push("https://www.xvideos.com" + $(this).find("a").attr("href"));
       });
-      $("div.mozaique > div > div.thumb-under").each(function (a, b) {
+
+      $("div.mozaique > div > div.thumb-under").each(function () {
         quality.push($(this).find("span.video-hd-mark").text());
       });
-      $("div.mozaique > div > div > div.thumb > a").each(function (a, b) {
+
+      $("div.mozaique > div > div > div.thumb > a").each(function () {
         thumb.push($(this).find("img").attr("data-src"));
       });
+
       for (let i = 0; i < title.length; i++) {
         hasil.push({
           title: title[i],
@@ -76,4 +80,4 @@ async function xvideosSearch(url) {
       resolve(hasil);
     });
   });
-};
+}
