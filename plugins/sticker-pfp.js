4bbdf14fa5954008746942f99ab9eae45c61e50b
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-13 10:24:47 -0500

    Actualizar sticker-pfp.js

--- plugins/sticker-pfp.js
+++ plugins/sticker-pfp.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
     let name = conn.getName(who);
     let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://qu.ax/QGAVS.jpg');
-    await conn.sendFile(m.chat, pp, 'profile.jpg', `*⛧ Foto de perfil de \`${name}\`*`, m);
+    await conn.sendFile(m.chat, pp, 'profile.jpg', `*🍪 Foto de perfil de \`${name}\`*`, m);
 }
 
 handler.help = ['pfp @user'];
