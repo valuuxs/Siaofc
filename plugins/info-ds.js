0fa5dc64d0cd331b935617982dc5f2cceea67b2f
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-13 23:06:09 -0500

    Actualizar info-ds.js

--- plugins/info-ds.js
+++ plugins/info-ds.js
@@ -1,6 +1,6 @@
 
-/* Codigo hecho por @Fabri115 y mejorado por BrunoSobrino */
-/*
+//Codigo hecho por @Fabri115 y mejorado por BrunoSobrino */
+
 import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs'
 import path from 'path'
 
@@ -40,40 +40,6 @@ await conn.reply(m.chat, '[ ℹ️ ] *Hola Soy \`ShadowBot\` Sigue el Canal*', m
 handler.help = ['ds', 'fixmsgespera']
 handler.tags = ['info']
 handler.command = /^(fixmsgespera|ds)$/i
-handler.estrellas = 5;
 handler.register = true
 
-export default handler*/
-
-
-// Tagall Mejorado por willzek
-import fetch from 'node-fetch';
-import PhoneNumber from 'awesome-phonenumber';
-
-const handler = async (m, { participants, args }) => {
-  const pesan = args.join` `;
-  const oi = `*» INFO :* ${pesan}`;
-  let mensajes = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄  ۪꒰ \`⡞᪲=͟͟͞Shadow Ultra ≼᳞ׄ\` ꒱  ۟  𝅄 ┄\n`;
-
-  for (const mem of participants) {
-    let numero = PhoneNumber('+' + mem.id.replace('@s.whatsapp.net', '')).getNumber('international');
-    let api = `https://delirius-apiofc.vercel.app/tools/country?text=${numero}`;
-    let response = await fetch(api);
-    let json = await response.json();
-
-    let paisdata = json.result ? json.result.emoji : '🍫';
-    mensajes += `${paisdata} @${mem.id.split('@')[0]}\n`;
-  }
-
-    mensajes += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄ ⸼`;
-
-  conn.sendMessage(m.chat, { text: mensajes, mentions: participants.map((a) => a.id) });
-};
-
-handler.help = ['todos *<mensaje opcional>*'];
-handler.tags = ['grupo'];
-handler.command = /^(tagall2|invocar2|marcar2|todos2|invocación2)$/i;
-handler.admin = true;
-handler.group = true;
-
-export default handler;
+export default handler
