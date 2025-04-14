1845fb70ee409522c787391e4ea2b4122b916d90
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-13 22:59:41 -0500

    Actualizar herramientas-readViewOnce.js

--- plugins/herramientas-readViewOnce.js
+++ plugins/herramientas-readViewOnce.js
@@ -1,20 +1,3 @@
-/*let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
-
-let handler = async (m, { conn }) => {
-if (!m.quoted) return conn.reply(m.chat, `*🍃 Por favor, responde a una imagen ViewOnce (ver solo vez)*.`, m)
-if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `*🍃 Responde a una imagen ViewOnce (ver solo una vez)*`, m)
-let buffer = await m.quoted.download(false);
-if (/videoMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
-} else if (/imageMessage/.test(m.quoted.mtype)) {
-return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m)
-}}
-handler.help = ['read']
-handler.tags = ['herramientas']
-handler.command = ['readviewonce', 'read', 'readvo', 'ver'] 
-handler.register = true 
-
-export default handler*/
 
 //Mejorado por Criss 
 
@@ -34,6 +17,6 @@ return conn.sendFile(m.chat, buffer, 'audio.mp3', '', m, { mimetype: 'audio/mp3'
 handler.help = ['read']
 handler.tags = ['herramientas']
 handler.command = ['readviewonce', 'read', 'readvo', 'ver'] 
-handler.register = true 
+//handler.register = true 
 
 export default handler;
