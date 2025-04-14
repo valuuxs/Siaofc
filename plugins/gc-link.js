c9ef75d2843daeec4da861add7a491584d65cfcf
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-13 21:41:59 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -2,7 +2,7 @@ var handler = async (m, { conn, args }) => {
 
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '🔗\v' + link, m, { detectLink: true })
+conn.reply(m.chat, '🌿```Link del Grupo:```\v' + link, m, { detectLink: true })
 
 }
 handler.help = ['link']
