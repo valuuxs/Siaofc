aa5c6688fa875510b0d84b0cb3986f843aa84d72
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-17 15:39:08 -0500

    Actualizar plugins

---
+++ plugins/jadibot-delsession.js
@@ -0,0 +1,24 @@
+import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fs} from "fs"
+import path, { join } from 'path'
+
+let handler  = async (m, { conn: parentw, usedPrefix, command}, args) => {
+
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let uniqid = `${who.split`@`[0]}`
+let userS = `${conn.getName(who)}`
+
+try {
+await fs.rmdir("./ShadowJadiBot/" + uniqid, { recursive: true, force: true })
+await parentw.sendMessage(m.chat, { text: '🚩 Sub-Bot eliminado.' }, { quoted: fkontak })
+} catch(err) {
+if (err.code === 'ENOENT' && err.path === `./CrowJadiBot/${uniqid}`) {
+await parentw.sendMessage(m.chat, { text: "🤍 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: fkontak })
+} else {
+await m.react(error)
+}}}
+handler.tags = ['serbot']
+handler.help = ['delsession']
+handler.command = /^(deletesess?ion|eliminarsesion|borrarsesion|delsess?ion|cerrarsesion|delserbot|logout)$/i
+handler.fail = null
+
+export default handler
