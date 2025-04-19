41a6d5467f5548fd2344898fac8a9bf7d3b6fa25
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-19 11:26:37 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -66,7 +66,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   return true
-}*/
+}
 
 
 import { WAMessageStubType } from '@whiskeysockets/baileys'
@@ -169,4 +169,85 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   return true
+}*/
+
+import { WAMessageStubType } from '@whiskeysockets/baileys';
+import fetch from 'node-fetch';
+import canvafy from 'canvafy';
+
+export async function before(m, { conn, participants, groupMetadata }) {
+  if (!m.messageStubType || !m.isGroup) return !0;
+
+  let chat = global.db.data.chats[m.chat];
+  let titu = 'SHADOW | WhatsApp Ai';
+  let grupo = 'https://chat.whatsapp.com/H5ueOzVRAzhBolt3lczDfG';
+  let who = m.messageStubParameters[0] + '@s.whatsapp.net';
+  let user = global.db.data.users[who];
+  let userName = user ? user.name : await conn.getName(who);
+
+  const getUserAvatar = async () => {
+    try {
+      return await conn.profilePictureUrl(m.messageStubParameters[0], 'image');
+    } catch (err) {
+      return 'https://i.ibb.co/cFzgdNw/file.jpg';
+    }
+  };
+
+  const generateImage = async (title, description, backgroundImage) => {
+    const userAvatar = await getUserAvatar();
+    const img = await new canvafy.WelcomeLeave()
+      .setAvatar(userAvatar)
+      .setBackground('image', backgroundImage)
+      .setTitle(title)
+      .setDescription(description)
+      .setBorder('#2a2e35')
+      .setAvatarBorder('#2a2e35')
+      .setOverlayOpacity(0.1)
+      .build();
+
+    return img;
+  };
+
+  let groupSize = participants.length;
+  if (m.messageStubType === 27) {
+    groupSize++;
+  } else if (m.messageStubType === 28 || m.messageStubType === 32) {
+    groupSize--;
+  }
+
+  if (chat.welcome && m.messageStubType == 27) {
+    let bienvenida = `❀ *Se unió* al grupo *${groupMetadata.subject.trim()}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]} \n\n    Ꮚ⁠˘⁠ ⁠ꈊ⁠ ⁠˘⁠ ⁠Ꮚ ¡Bienvenido! ¡Esperamos que tengas un excelente día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 ¡Disfruta de tu tiempo con nosotros!`;
+
+    let img = await generateImage(
+      '¡BIENVENIDO/A!',
+      `Disfruta de tu estadía. Ahora somos ${groupSize} miembros.`,
+      'https://i.ibb.co/1fVJfvxk/file.jpg'
+    );
+
+    await conn.sendMini(m.chat, titu, null, bienvenida, img, img, grupo, null);
+  }
+
+  if (chat.welcome && m.messageStubType == 28) {
+    let bye = `❀ *Se salió* del grupo  *${groupMetadata.subject.trim()}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    Ꮚ⁠˘⁠ ⁠ꈊ⁠ ⁠˘⁠ ⁠Ꮚ ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Próximamente...`;
+
+    let img = await generateImage(
+      '¡HASTA LUEGO!',
+      `Nos vemos pronto. Ahora somos ${groupSize} miembros.`,
+      'https://i.ibb.co/Kcf0xdrQ/file.jpg'
+    );
+
+    await conn.sendMini(m.chat, titu, null, bye, img, img, grupo, null);
+  }
+
+  if (chat.welcome && m.messageStubType == 32) {
+    let kick = `❀ *Se salió* del grupo  *${groupMetadata.subject.trim()}*\n    ✰ @${m.messageStubParameters[0].split`@`[0]}\n\n    Ꮚ⁠˘⁠ ⁠ꈊ⁠ ⁠˘⁠ ⁠Ꮚ ¡Nos vemos pronto! ¡Que tengas un buen día!\n\n> ✐ No olvides usar *#help* si necesitas algo.\n> 🜸 Próximamente...`;
+
+    let img = await generateImage(
+      '¡HASTA LUEGO!',
+      `Nos vemos pronto. Ahora somos ${groupSize} miembros.`,
+      'https://i.ibb.co/Kcf0xdrQ/file.jpg'
+    );
+
+    await conn.sendMini(m.chat, titu, null, kick, img, img, grupo, null);
+  }
 }
