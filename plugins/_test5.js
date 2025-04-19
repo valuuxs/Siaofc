447666722072ca7e84d578e1b2db115cfe2139a9
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-19 11:33:37 -0500

    Actualizar _test5.js

--- plugins/_test5.js
+++ plugins/_test5.js
@@ -1,36 +1,34 @@
-import fetch from 'node-fetch';
-
-const handler = async (m, { conn, text, usedPrefix, command }) => {
-  try {
-    const insta = 'https://instagram.com/dev.criss_vx';
-    const img = 'https://files.catbox.moe/xr2m6u.jpg'; // reemplaza con tu imagen
-    const mensaje = `Hola @${m.sender.split('@')[0]}, gracias por usar *Shadow Bot*!\n\nSigue al Dev Criss en Instagram!`;
-
-    await conn.sendMessage(m.chat, {
-      text: mensaje,
-      contextInfo: {
-        mentionedJid: [m.sender],
-        isForwarded: true,
-        forwardingScore: 999,
-        externalAdReply: {
-          title: `${await conn.getName(m.sender)}, gracias por usar Shadow Bot`,
-          body: 'Desarrollado por Dev Criss',
-          thumbnail: await (await fetch(img)).buffer(),
-          sourceUrl: insta,
-          mediaType: 1,
-          renderLargerThumbnail: true
-        }
-      }
-    }, { quoted: m });
-    
-  } catch (e) {
-    console.error(e);
-    conn.reply(m.chat, '❎ Ocurrió un error al enviar el mensaje.', m);
+export default {
+  async before(m, { conn }) {
+    if (!conn.sendMini) {
+      conn.sendMini = async function (
+        jid,
+        title,
+        body = '',
+        text = '',
+        thumbnailUrl = '',
+        thumbnail = '',
+        sourceUrl = '',
+        quoted = null,
+        LargerThumbnail = true
+      ) {
+        return conn.sendMessage(jid, {
+          text,
+          contextInfo: {
+            mentionedJid: await conn.parseMention(text),
+            externalAdReply: {
+              title: title,
+              body: body,
+              mediaType: 1,
+              previewType: 0,
+              renderLargerThumbnail: LargerThumbnail,
+              thumbnailUrl: typeof thumbnailUrl === 'string' ? thumbnailUrl : undefined,
+              thumbnail: thumbnail instanceof Buffer ? thumbnail : undefined,
+              sourceUrl: sourceUrl
+            }
+          }
+        }, { quoted });
+      };
+    }
   }
 };
-
-handler.command = /^promo$/i;
-handler.help = ['promo'];
-handler.tags = ['info'];
-
-export default handler;
