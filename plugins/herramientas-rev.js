77b37fb2939d23873d9c2eb5fd5936b1f5bfe436
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-19 10:21:39 -0500

    Actualizar herramientas-rev.js

--- plugins/herramientas-rev.js
+++ plugins/herramientas-rev.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch'
-import { sendMini } from '../plugins/_test5.js'
+import { sendMini } from '../lib/sendMini.js'
 
 let handler = async (m, { conn }) => {
   let img = await (await fetch('https://i.imgur.com/RP6QwCS.jpeg')).buffer()
-  await sendMini(conn, m.chat, 'Ver Proyecto', '¡Este es un mensaje de prueba con sendMini!', 'Shadow Bot', img, 'https://github.com/CrxstianEscobar/ShadowUltra-MD', m)
+  await sendMini(conn, m.chat, 'Ir al Proyecto', 'Este es un test de *sendMini* adaptado.', 'Shadow Bot', img, 'https://github.com/CrxstianEscobar/ShadowUltra-MD', m)
 }
 
 handler.command = /^testmini$/i
