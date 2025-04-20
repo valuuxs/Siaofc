487f963bacd9823534512cf14072345027bc7799
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-20 15:56:49 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -10,14 +10,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
   let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg'
-  let insta = 'https://instagram.com/dev.criss_vx'
 
-let groupSize = participants.length
-if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) groupSize++
-if (
-  m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE ||
-  m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE
-) groupSize--
 
   if (chat.welcome) {
     let img
@@ -132,7 +125,7 @@ if (
 }
 */
 //⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
-/*
+
 import { WAMessageStubType } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
@@ -141,14 +134,13 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
   let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
-  let groupSize = participants.length
-  if (m.messageStubType == 27) {
-    groupSize++;
-  } if (m.messageStubType == 28) {
-    groupSize--;
-  } if (m.messageStubType == 32) {
-    groupSize--;
-  }
+let groupSize = participants.length
+if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) groupSize++
+if (
+  m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE ||
+  m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE
+) groupSize--
+  let insta = 'https://instagram.com/dev.criss_vx'
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
@@ -159,26 +151,46 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let member1 = 'ゲ◜៹ Bye Member ៹◞ゲ'
   let member2 = 'ゲ◜៹ Kicked Member ៹◞ゲ'
 
+  const groupName = groupMetadata.subject
+  const groupDesc = groupMetadata.desc || 'sin descripción'
 
-  if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `*¡Bienvenido(a)!*\n෫ࣲׄ֟፝͡${taguser} 🫶🏻꒱\n\nᦷᩘᦷ   ݂ 𝖣𝗂𝗌𝖿𝗋𝗎𝗍𝖺 𝖽𝖾 𝗍𝗎 𝖾𝗌𝗍𝖺𝖽𝗂𝖺.\n✎ 𝖴𝗌𝖺 *#help* 𝗉𝖺𝗋𝖺 𝗏𝖾𝗋 𝗅𝖺 𝗅𝗂𝗌𝗍𝖺 𝖽𝖾 𝖼𝗈𝗆𝖺𝗇𝖽𝗈𝗌.\n\n> ${dev}`    
-    await conn.sendLuffy(m.chat, txt, member, bienvenida, img, img, redes, fkontak)
+    // 𝗕𝗜𝗘𝗡𝗩𝗘𝗡𝗜𝗗𝗔 𝗦𝗛𝗔𝗗𝗢𝗪 𝗨𝗟𝗧𝗥𝗔 🤍
+    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+      let text = chat.sWelcome
+        ? chat.sWelcome
+            .replace(/@user/g, taguser)
+            .replace(/@group/g, groupName)
+            .replace(/@desc/g, groupDesc)
+            .replace(/@count/g, groupSize)
+       : `*¡Bienvenido(a)!*\n෫ࣲׄ֟፝͡${taguser} 🫶🏻꒱\n\nᦷᩘᦷ   ݂ 𝖣𝗂𝗌𝖿𝗋𝗎𝗍𝖺 𝖽𝖾 𝗍𝗎 𝖾𝗌𝗍𝖺𝖽𝗂𝖺.\n✎ 𝖴𝗌𝖺 *#help* 𝗉𝖺𝗋𝖺 𝗏𝖾𝗋 𝗅𝖺 𝗅𝗂𝗌𝗍𝖺 𝖽𝖾 𝖼𝗈𝗆𝖺𝗇𝖽𝗈𝗌.\n\n> ${dev}`    
+    await conn.sendLuffy(m.chat, txt, member, bienvenida, img, img, insta, fkontak)
   }
 
-  if (chat.welcome && m.messageStubType == 28)
-{
-    let bye = `*¡Hasta Luego!*\n෫ࣲׄ֟፝͡${taguser} 👋🏻꒱\n\nᏊ⁠˘⁠ ⁠ꈊ⁠ ⁠˘⁠ ⁠Ꮚ 𝖦𝗋𝖺𝖼𝗂𝖺𝗌 𝗉𝗈𝗋 𝖾𝗌𝗍𝖺𝗋 𝖺𝗊𝗎𝗂. 𝖳𝗎 𝗉𝗋𝖾𝗌𝖾𝗇𝖼𝗂𝖺 𝖿𝗎𝖾 𝗎𝗇 𝖾𝗌𝗍𝗈𝗋𝖻𝗈.\n ׅ⿻ 𝖮𝗃𝖺𝗅𝖺 𝗒 𝗌𝖾 𝗅𝗈 𝗏𝗂𝗈𝗅𝖾𝗇 𝗅𝗈𝗌 𝗇𝖾𝗀𝗋𝗈𝗌 𝗉𝗈𝗋 𝖺𝗅𝗍𝖺 𝗉𝗎𝗍𝖺.\n\n> ${dev}`
-    await conn.sendLuffy(m.chat, txt1, member1, bye, img, img, redes, fkontak)
+    // 𝗦𝗔𝗟𝗜𝗗𝗔 𝗦𝗛𝗔𝗗𝗢𝗪 𝗨𝗟𝗧𝗥𝗔 🤍
+    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
+      let text = chat.sBye
+            .replace(/@user/g, taguser)
+            .replace(/@group/g, groupName)
+            .replace(/@desc/g, groupDesc)
+            .replace(/@count/g, groupSize)
+       : `*¡Hasta Luego!*\n෫ࣲׄ֟፝͡${taguser} 👋🏻꒱\n\nᏊ⁠˘⁠ ⁠ꈊ⁠ ⁠˘⁠ ⁠Ꮚ 𝖦𝗋𝖺𝖼𝗂𝖺𝗌 𝗉𝗈𝗋 𝖾𝗌𝗍𝖺𝗋 𝖺𝗊𝗎𝗂. 𝖳𝗎 𝗉𝗋𝖾𝗌𝖾𝗇𝖼𝗂𝖺 𝖿𝗎𝖾 𝗎𝗇 𝖾𝗌𝗍𝗈𝗋𝖻𝗈.\n ׅ⿻ 𝖮𝗃𝖺𝗅𝖺 𝗒 𝗌𝖾 𝗅𝗈 𝗏𝗂𝗈𝗅𝖾𝗇 𝗅𝗈𝗌 𝗇𝖾𝗀𝗋𝗈𝗌 𝗉𝗈𝗋 𝖺𝗅𝗍𝖺 𝗉𝗎𝗍𝖺.\n\n> ${dev}`
+    await conn.sendLuffy(m.chat, txt1, member1, bye, img, img, insta, fkontak)
  }
 
-if (chat.welcome && m.messageStubType == 32)
-{
-    let kick = `*¡Expulsado!*\n෫ࣲׄ֟፝͡${taguser} 👊🏻꒱\n\nᏊ⁠ 𝖴𝗇 𝗇𝖾𝗀𝗋𝗈 𝗆𝖾𝗇𝗈𝗌 𝖾𝗇 𝖾𝗅 𝗀𝗋𝗎𝗉𝗈, 𝗉𝗈𝗋 𝗇𝗈 𝗈𝖻𝖾𝖽𝖾𝖼𝖾𝗋 𝗅𝖺𝗌 𝗋𝖾𝗀𝗅𝖺𝗌.\n ׅ⿻ 𝖮𝗃𝖺𝗅𝖺 𝗒 𝗅𝖺 𝖾𝗅𝗂𝗆𝗂𝗇𝖺𝖼𝗂𝗈𝗇 𝗅𝖾 𝗁𝖺𝗀𝖺 𝗋𝖾𝖿𝗅𝖾𝗑𝗂𝗈𝗇𝖺𝗋 𝗑𝖣\n\n> ${dev}`
-    await conn.sendLuffy(m.chat, txt2, member2, kick, img, img, redes, fkontak)
+    // 𝗘𝗫𝗣𝗨𝗟𝗦𝗜𝗢́𝗡 𝗦𝗛𝗔𝗗𝗢𝗪 𝗨𝗟𝗧𝗥𝗔 🤍
+    if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE) {
+      let text = chat.sRemove
+        ? chat.sRemove
+            .replace(/@user/g, taguser)
+            .replace(/@group/g, groupName)
+            .replace(/@desc/g, groupDesc)
+            .replace(/@count/g, groupSize)
+        : `*¡Expulsado!*\n෫ࣲׄ֟፝͡${taguser} 👊🏻꒱\n\nᏊ⁠ 𝖴𝗇 𝗇𝖾𝗀𝗋𝗈 𝗆𝖾𝗇𝗈𝗌 𝖾𝗇 𝖾𝗅 𝗀𝗋𝗎𝗉𝗈, 𝗉𝗈𝗋 𝗇𝗈 𝗈𝖻𝖾𝖽𝖾𝖼𝖾𝗋 𝗅𝖺𝗌 𝗋𝖾𝗀𝗅𝖺𝗌.\n ׅ⿻ 𝖮𝗃𝖺𝗅𝖺 𝗒 𝗅𝖺 𝖾𝗅𝗂𝗆𝗂𝗇𝖺𝖼𝗂𝗈𝗇 𝗅𝖾 𝗁𝖺𝗀𝖺 𝗋𝖾𝖿𝗅𝖾𝗑𝗂𝗈𝗇𝖺𝗋 𝗑𝖣\n\n> ${dev}`
+    await conn.sendLuffy(m.chat, txt2, member2, kick, img, img, insta, fkontak)
 
   }}
 ⭐⭐⭐⭐
-*/
+/*
 
 
 import { WAMessageStubType } from '@whiskeysockets/baileys';
@@ -260,4 +272,4 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
     await conn.sendLuffy(m.chat, titu, null, kick, img, img, grupo, null);
   }
-}
+}*/
