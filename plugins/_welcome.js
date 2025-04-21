e58267034ef5a844c433523945fd56a823f81d9e
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-20 21:10:08 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,4 +1,4 @@
-import { WAMessageStubType } from '@whiskeysockets/baileys'
+/*import { WAMessageStubType } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, { conn, participants, groupMetadata }) {
@@ -9,11 +9,6 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let chat = global.db.data.chats[m.chat]
   let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg'
   let insta = 'https://instagram.com/dev.criss_vx'
-/*
-  let groupSize = participants.length
-  if (m.messageStubType === 27) groupSize++
-  else if (m.messageStubType === 28 || m.messageStubType === 32) groupSize--
-*/
 let groupSize = participants.length
 if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) groupSize++
 if (
@@ -131,4 +126,33 @@ if (
   }
 
   return true
-}
+}*/
+
+
+import { WAMessageStubType } from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
+
+export async function before(m, { conn, participants, groupMetadata }) {
+  if (!m.messageStubType || !m.isGroup) return true
+
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
+  let img = await (await fetch(`${pp}`)).buffer()
+  let chat = global.db.data.chats[m.chat]
+  let txt = 'ゲ◜៹ New Member ៹◞ゲ'
+  let txt1 = 'ゲ◜៹ Bye Member ៹◞ゲ'
+  let groupSize = participants.length
+  if (m.messageStubType == 27) {
+    groupSize++;
+  } else if (m.messageStubType == 28 || m.messageStubType == 32) {
+    groupSize--;
+  }
+
+  if (chat.welcome && m.messageStubType == 27) {
+    let bienvenida = `❀ *Bienvenido* a ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`    
+    await conn.sendLuffy(m.chat, txt, dev, bienvenida, img, img, redes, fkontak)
+  }
+
+  if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
+    let bye = `❀ *Adiós* de ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+    await conn.sendLuffy(m.chat, txt1, dev, bye, img, img, redes, fkontak)
+  }}
