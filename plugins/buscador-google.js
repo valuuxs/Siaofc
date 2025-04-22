69c3b77e192b958d797fef7037f0df417de3ae3e
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-22 18:47:16 -0500

    Actualizar buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -2,34 +2,33 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { text }) => {
   if (!text) {
-    m.reply(`${emoji} Por favor, proporciona el termino de búsqueda que deseas realizar a *Google*.`);
+    m.reply(`*🔎 Por favor, proporciona el término de búsqueda que deseas realizar en Google*.`);
     return;
   }
 
-  const apiUrl = `https://delirius-apiofc.vercel.app/search/googlesearch?query=${encodeURIComponent(text)}`;
+  const apiUrl = `https://vapis.my.id/api/googlev1?q=${encodeURIComponent(text)}`;
 
   try {
     const response = await fetch(apiUrl);
     const result = await response.json();
 
     if (!result.status) {
-      m.reply('Error al realizar la búsqueda.');
+      m.reply('*⚠️ Error al realizar la búsqueda.*');
       return;
     }
 
-    let replyMessage = `${emoji2} Resultados de la búsqueda:\n\n`;
-    result.data.slice(0, 1).forEach((item, index) => {
+    let replyMessage = `Resultados de la búsqueda:\n\n`;
+    result.data.forEach((item, index) => {
       replyMessage += `☁️ *${index + 1}. ${item.title}*\n`;
-      replyMessage += `📰 *${item.description}*\n`;
-      replyMessage += `🔗 URL: ${item.url}`;
+      replyMessage += `📰 *${item.desc}*\n`;
+      replyMessage += `🔗 URL: ${item.link}\n\n`;
     });
 
-m.react('✅')
-
+    m.react('✅');
     m.reply(replyMessage);
   } catch (error) {
-    console.error(`${msm} Error al realizar la solicitud a la API:`, error);
-    m.reply(`${msm} Ocurrió un error al obtener los resultados.`);
+    console.error(`Error al realizar la solicitud a la API:`, error);
+    m.reply(`Ocurrió un error al obtener los resultados.`);
   }
 };
 
