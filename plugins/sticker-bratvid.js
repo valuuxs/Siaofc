c761736c307abedf0b6011d3239d49961d796f86
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-26 22:33:37 -0500

    Actualizar sticker-bratvid.js

--- plugins/sticker-bratvid.js
+++ plugins/sticker-bratvid.js
@@ -1,4 +1,4 @@
-/*
+
 import { sticker } from '../lib/sticker.js'
 import axios from 'axios'
 
@@ -11,7 +11,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
     let url = `https://api.nekorinn.my.id/maker/bratvid?text=${encodeURIComponent(query)}`
     let res = await axios.get(url, {
       responseType: 'arraybuffer',
-      timeout: 10000
+      timeout: 1000000
     })
     let contentType = res.headers['content-type']
     if (!contentType || !contentType.startsWith('video/')) throw m.reply('error en la API.')
@@ -34,7 +34,7 @@ handler.tags = ['sticker']
 handler.limit = false
 
 export default handler
-*/
+/*
 
 import axios from 'axios';
 import fs from 'fs';
@@ -90,4 +90,4 @@ handler.command = ['bratvid'];
 handler.tags = ['sticker'];
 handler.help = ['bratvid <texto>'];
 
-export default handler;
+export default handler;*/
