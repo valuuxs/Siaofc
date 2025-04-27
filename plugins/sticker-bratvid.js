42b61db1440e49bf56238de9cdb113b89849de9d
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-04-26 22:55:20 -0500

    Actualizar sticker-bratvid.js

--- plugins/sticker-bratvid.js
+++ plugins/sticker-bratvid.js
@@ -34,31 +34,29 @@ handler.tags = ['sticker']
 
 export default handler*/
 
-
 import { sticker } from '../lib/sticker.js'
 import axios from 'axios'
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
   if (!text) {
-    return conn.reply(m.chat, '*🍪 Por favor, ingresa un texto para realizar tu sticker.*', m, rcanal)
-}
+    return conn.reply(m.chat, '*🍪 Por favor, ingresa un texto para realizar tu sticker.*\n> *\`Ejemplo:\`* Hello Word', m, rcanal)
+  }
 
   m.react('⏳')
 
   try {
-    let url = `https://api.nekorinn.my.id/maker/bratvid?text=${encodeURIComponent(query)}`
-    let res = await axios.get(url)
+    let url = `https://api.nekorinn.my.id/maker/bratvid?text=${encodeURIComponent(text)}`
+    let res = await axios.get(url, { responseType: 'arraybuffer' })
     let contentType = res.headers['content-type']
-    if (!contentType || !contentType.startsWith('video/')) throw m.reply('error en la API.')
+    if (!contentType || !contentType.startsWith('video/')) throw new Error('Error en la API.')
 
-   
     let bratSticker = await sticker(res.data, null, global.packname, global.author)
-    
-    
-    await conn.sendFile(m.chat, bratSticker, null, { asSticker: true }, m)
+
+    await conn.sendMessage(m.chat, { sticker: bratSticker }, { quoted: m })
     m.react('✅')
   } catch (err) {
     console.error(err)
+    m.react('❌')
     m.reply(`❌ Error: ${err.message}`)
   }
 }
