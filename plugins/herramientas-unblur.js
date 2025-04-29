8b1e0b52e5d05787d0d92cecd2d2076592279036
KenisawaDevolper <137190861+MauroAzcurra@users.noreply.github.com>
2025-04-29 10:03:41 -0300

    Add files via upload

---
+++ plugins/herramientas-unblur.js
@@ -0,0 +1,71 @@
+import fetch from "node-fetch";
+import crypto from "crypto";
+import { FormData, Blob } from "formdata-node";
+import { fileTypeFromBuffer } from "file-type";
+import axios from 'axios';
+import FormData from 'form-data';
+
+const handler = async (m, { conn }) => {
+let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || "";
+  if (!mime) return m.reply("No media found", null, { quoted: fkontak });
+  let media = await q.download();
+let link = await catbox(media);
+let type = (args[0] === 'mild') ? 'mild' : 'unblur';
+await m.react("🐢")
+let { data } = await axios.get(`https://abella.icu/unblur?url=${encodeURIComponent(link)}&type=${type}`, {
+      responseType: 'arraybuffer'
+    });
+// await conn.sendFile(m.chat, Buffer.from(data), 'thumbnail.jpg', `Listo 🌟`, m, null, rcanal);
+await conn.sendMessage(m.chat, {image: { url: Buffer.from(data) }}, {quoted: fkontak});
+}
+handler.help = ['unblur <unblur/mild>']
+handler.tags = ['inteligencia']
+handler.command = ['unblur']
+export default handler
+
+
+function formatBytes(bytes) {
+  if (bytes === 0) {
+    return "0 B";
+  }
+  const sizes = ["B", "KB", "MB", "GB", "TB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(1024));
+  return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
+}
+
+
+/**
+ * Upload image to catbox
+ * Supported mimetype:
+ * - `image/jpeg`
+ * - `image/jpg`
+ * - `image/png`s
+ * - `image/webp`
+ * - `video/mp4`
+ * - `video/gif`
+ * - `audio/mpeg`
+ * - `audio/opus`
+ * - `audio/mpa`
+ * @param {Buffer} buffer Image Buffer
+ * @return {Promise<string>}
+ */
+async function catbox(content) {
+  const { ext, mime } = (await fileTypeFromBuffer(content)) || {};
+  const blob = new Blob([content.toArrayBuffer()], { type: mime });
+  const formData = new FormData();
+  const randomBytes = crypto.randomBytes(5).toString("hex");
+  formData.append("reqtype", "fileupload");
+  formData.append("fileToUpload", blob, randomBytes + "." + ext);
+
+  const response = await fetch("https://catbox.moe/user/api.php", {
+    method: "POST",
+    body: formData,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+    },
+  });
+
+  return await response.text();
+}
