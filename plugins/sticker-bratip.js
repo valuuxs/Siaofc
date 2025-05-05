81049b98ddbf914d3e86ca7658a32fa73db12a73
KenisawaDevolper <137190861+MauroAzcurra@users.noreply.github.com>
2025-05-04 23:07:29 -0300

    Add files via upload

---
+++ plugins/sticker-bratip.js
@@ -0,0 +1,18 @@
+
+import { sticker } from '../lib/sticker.js' 
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw m.reply(`Ejemplo: ${usedPrefix + command} hola mundo`)
+  let url = `https://api.nekorinn.my.id/maker/brat-v2?text=${encodeURIComponent(text)}`
+  let stiker = await sticker(null, url, 'cmd by', 'kenisawaDev') 
+  
+  await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+}
+
+handler.help = ['brat <texto>']
+handler.tags = ['sticker']
+handler.command = /^brat$/i
+handler.group = false
+handler.limit = true
+
+export default handler
