20d61a07976266588708f62b319c18492b57cb23
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-05-04 22:53:40 -0500

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -9,7 +9,7 @@ const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const fetchSticker = async (text, attempt = 1) => {
     try {
-        const response = await axios.get('https://api.nekorinn.my.id/maker/brat-v2', {
+        const response = await axios.get('https://vapis.my.id/api/bratv1', {
             params: {
                 q: text
             },
