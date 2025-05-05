0004e1fe9648acb5e95520631a56be99f2b94917
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-05 18:21:57 -0500

    Actualizar nsfw-manosear.js

--- plugins/nsfw-manosear.js
+++ plugins/nsfw-manosear.js
@@ -4,7 +4,7 @@ import path from 'path';
 let handler = async (m, { conn, usedPrefix }) => {
     let who;
     if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-    return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
+    return m.reply(hotw);
     }
     // Verificamos si se menciona a alguien o se cita un mensaje
     if (m.mentionedJid.length > 0) {
