dab2c950ae236fca5d81727a4b8e7fd7090d8ffc
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-05 18:25:51 -0500

    Actualizar nsfw-pack.js

--- plugins/nsfw-pack.js
+++ plugins/nsfw-pack.js
@@ -1,29 +1,8 @@
-/*import fetch from 'node-fetch';
-
-import fetch from 'node-fetch';
-
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-
-m.react('🕑');
-
-let txt = 'Pack🔥🔥🔥\n> Pon De Nuevo .pack para mirar el siguiente ✨';
-
-let img = 'https://delirius-apiofc.vercel.app/nsfw/girls';
-
-m.react('✅');
-conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: fkontak });
-}
-
-handler.command = ['pack'];
-
-export default handler;
-
-
 import fetch from 'node-fetch';*/
 
 let handler = async (m, { conn, usedPrefix, command }) => {
 
-    //m.react('🕑');
+    m.react('💋');
 
     let txt = 'Pack 🔥';
     let img = 'https://delirius-apiofc.vercel.app/nsfw/girls';
