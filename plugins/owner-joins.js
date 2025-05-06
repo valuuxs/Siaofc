fc6c92305eac56ee1e78ccd86337f08519ef1226
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-05 22:17:12 -0500

    Actualizar owner-joins.js

--- plugins/owner-joins.js
+++ plugins/owner-joins.js
@@ -1,36 +1,16 @@
-/*let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
-
-let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
-
-if (!text) return m.reply(`[ ℹ️ ] Ingresa el enlace del Grupo.*`)
-try {
-let [_, code] = text.match(linkRegex) || []
-if (!code) return m.reply('*[ ⚠️ ] Enlace inválido.*')
-let res = await conn.groupAcceptInvite(code)
-m.reply(`*[ ℹ️ ] Me uní correctamente al Grupo.*`)
-} catch (e) {
-return m.reply(`✘ Ocurrió un error. ${e.message}`)}}
-
-handler.help = ['join <link>']
-handler.tags = ['owner']
-handler.command = ['join', 'entrar'] 
-handler.rowner = true
-
-export default handler*/
-
 //Mejorado por Criss Escobar
 
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})(?:\s+([0-9]{1,3}))?/i
 
 let handler = async (m, { conn, text }) => {
-    if (!text) return m.reply(`*[ ℹ️ ] Ingresa el enlace del Grupo.*`)
+    if (!text) return m.reply(`🌿 Por favor, ingresa el enlace del Grupo.*`)
 
     try {
         let [_, code, expired] = text.match(linkRegex) || []
-        if (!code) return m.reply('*[ ⚠️ ] enlace inválido.*')
+        if (!code) return m.reply('*⚠️ enlace inválido.*')
 
         let res = await conn.groupAcceptInvite(code)
-        m.reply(`*[ ✅ ] Shadow se unió correctamente al grupo.*\n*¡Disfruta del Bot en tu grupo!*`)
+        m.reply(`*✅ Shadow se unió correctamente al grupo.*\n*¡Disfruta del Bot en tu grupo!*`)
 
 /*let groupId = await conn.groupAcceptInvite(code)
 
@@ -44,10 +24,10 @@ m.reply(`*[ ✅ ] Shadow se unió correctamente al grupo.*\n*¡Disfruta del Bot 
             expired = Math.min(999, Math.max(1, isNumber(expired) ? parseInt(expired) : 0))
             let chats = global.db.data.chats[res] || (global.db.data.chats[res] = {})
             chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
-            m.reply(`*[ ⌛ ] Shadow permanecerá en el grupo durante \`${expired}\` días.*`)
+            m.reply(`*⌛ Shadow permanecerá en el grupo durante \`${expired}\` días.*`)
         }
     } catch {
-        return m.reply(`*[ ❌ ] Ocurrió un error al otrar al grupo.*`) 
+        return m.reply(`*❌ Ocurrió un error al otrar al grupo.*`) 
     }
 }
 
