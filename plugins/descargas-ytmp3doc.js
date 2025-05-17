9333d4ad150cf73e1394642ea109e30077174fa8
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-17 17:42:15 -0500

    Actualizar descargas-ytmp3doc.js

--- plugins/descargas-ytmp3doc.js
+++ plugins/descargas-ytmp3doc.js
@@ -40,7 +40,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
       document: { url: apiData.download.url },
       mimetype: "audio/mpeg",
       fileName: `${video.title}.mp3`,
-      caption: `\`\`\`◜YouTube - MP3◞\`\`\`\n*${video.title}*`,
+      caption: `\`\`\`◜YouTube - MP3◞\`\`\`\n\n*${video.title}*`,
     };
 
     await conn.sendMessage(m.chat, audioMessage, { quoted: m });
