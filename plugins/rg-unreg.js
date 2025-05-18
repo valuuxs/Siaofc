ca28fc138c625cffb6c116a896c177cec4cc8337
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-18 17:51:08 -0500

    Actualizar rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `*[ ℹ️ ] Registro eliminado.*\n*Usted ya no está en mi base de datos*`, m, rcanal)
+return conn.reply(m.chat, `*🚮 Tu registro fue eliminado.*`, m, fkontak)
 
 }
 handler.help = ['unreg']
