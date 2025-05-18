477ffaa373207b8736374120bff2063d8d29d000
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-18 14:28:35 -0500

    Actualizar rpg-slut.js

--- plugins/rpg-slut.js
+++ plugins/rpg-slut.js
@@ -4,7 +4,7 @@ let user = global.db.data.users[m.sender]
 let tiempo = 5 * 60
 if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
 const tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-conn.reply(m.chat, `*⏰ Espera \`${tiempo2}\` para seguir prostituyendote.*`, m)
+conn.reply(m.chat, `*⏰ Debes esperar \`${tiempo2}\` para seguir prostituyendote.*`, m)
 return
 }
 let rsl = Math.floor(Math.random() * 5000)
