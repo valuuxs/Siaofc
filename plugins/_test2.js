c5a1ab0f8074481f6911e78681acc7bf4fc3f2af
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-17 22:46:28 -0500

    Actualizar _test2.js

---
+++ plugins/_test2.js
@@ -0,0 +1,26 @@
+const handler = async (m, { conn, text }) => {
+  if (!text) throw '*[❗] Ingresa el mensaje a enviar con la ubicación*';
+
+  const mensaje = '[❗𝐋𝐈𝐕𝐄 𝐓𝐄𝐒𝐓❗]\n\n' + text + '\n\nEste es un test de liveLocationMessage';
+
+  await conn.relayMessage(m.chat, {
+    liveLocationMessage: {
+      degreesLatitude: 35.685506276233525,
+      degreesLongitude: 139.75270667105852,
+      accuracyInMeters: 0,
+      degreesClockwiseFromMagneticNorth: 2,
+      caption: mensaje,
+      sequenceNumber: 2,
+      timeOffset: 3,
+    },
+  }, {}).catch(e => m.reply('*[⚠️] Error al enviar liveLocationMessage:* ' + e));
+
+  m.reply('*[✅] Mensaje de ubicación en vivo enviado exitosamente.*');
+};
+
+handler.help = ['testlive <mensaje>'];
+handler.tags = ['test'];
+handler.command = /^testlive$/i;
+handler.owner = true;
+
+export default handler;
