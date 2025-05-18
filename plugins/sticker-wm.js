8901dd0167134e452050dfb57a1142c101cb991a
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-18 00:26:43 -0500

    Actualizar sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,34 +1,10 @@
-/*import { addExif } from '../lib/sticker.js'
-let handler = async (m, { conn, text }) => {
-  if (!m.quoted) return conn.reply(m.chat, '*🍪 Por favor, responde al sticker.*', m, rcanal)
-  let stiker = false
-  try {
-    let [packname, ...author] = text.split('|')
-    author = (author || []).join('|')
-    let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) return m.reply(`*⚠️ El archivo adjunto no es un sticker, por favor responde a un sticker.*`)
-    let img = await m.quoted.download()
-    if (!img) return m.reply(`*🍪 Responde al sticker.*`)
-    stiker = await addExif(img, packname || '', author || '')
-  } catch (e) {
-    console.error(e)
-    if (Buffer.isBuffer(e)) stiker = e
-  } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m)
-    else return m.reply(`*🍪 Responde al sticker.*`)
-  }
-}
-handler.help = ['wm']
-handler.tags = ['sticker']
-handler.command = ['take', 'wm']
 
-export default handler*/
 
 import { addExif } from '../lib/sticker.js'
 
 let handler = async (m, { conn, text }) => {
 
-  if (!m.quoted) return conn.reply(m.chat, '*🍪 Por favor, responde al sticker.*', m, rcanal)
+  if (!m.quoted) return conn.reply(m.chat, `*${xsticker} Por favor, responde al sticker.*`, m, rcanal)
   
   let stiker = false
   try {
@@ -42,7 +18,7 @@ let handler = async (m, { conn, text }) => {
     
     // Descarga el archivo citado
     let img = await m.quoted.download()
-    if (!img) return conn.reply(m.chat, '*🍪 Responde al sticker.*', m)
+    if (!img) return conn.reply(m.chat, `*${xsticker} Responde al sticker.*`, m)
     
     // Se añade el EXIF al sticker
     stiker = await addExif(img, packname || '', author || '')
@@ -52,7 +28,7 @@ let handler = async (m, { conn, text }) => {
   } finally {
     // Si se ha creado el sticker, lo envía
     if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, rcanal)
-    else return conn.reply(m.chat, '*🍪 Responde al sticker.*', m)
+    else return conn.reply(m.chat, `*${xsticker} Responde al sticker.*`, m)
   }
 }
 
