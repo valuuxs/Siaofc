c91608d2c3d10c52589601301f884eb634eb6089
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-17 23:52:18 -0500

    Actualizar fun-genio.js

--- plugins/fun-genio.js
+++ plugins/fun-genio.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-  if (!text) return conn.reply(m.chat, `*${xfun} Por favor, ingresa la pregunta\n> *\`Ejemplo:\`* ${usedPrefix + command} La admin es virgen?`, m)
+  if (!text) return conn.reply(m.chat, `*${xfun} Por favor, ingresa la pregunta*\n> *\`Ejemplo:\`* ${usedPrefix + command} La admin es virgen?`, m)
 
   const respuestas = [
     'Sí.',
