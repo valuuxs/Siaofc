f13487bf965878894dc126b82e182d6d96473dbf
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-19 08:05:53 -0500

    Actualizar gc-groupdesc.js

--- plugins/gc-groupdesc.js
+++ plugins/gc-groupdesc.js
@@ -2,15 +2,15 @@ const handler = async (m, { conn, args }) => {
   const text = args.join(' ').trim();
 
   if (!text) {
-    return m.reply('*[ ℹ️ ] Debes proporcionar una nueva descripción para el grupo.*');
+    return m.reply(`*${xgc} Debes proporcionar una nueva descripción para el grupo.*`);
   }
 
   try {
     await conn.groupUpdateDescription(m.chat, text);
-    m.reply('*[ ✅ ] La descripción del grupo se modificó correctamente.*');
+    m.reply('*✅ La descripción del grupo se modificó correctamente.*');
   } catch (error) {
     console.error(error);
-    m.reply('*[ ❌ ] Ocurrió un error al actualizar la descripción del grupo.*');
+    m.reply('*✖️ Ocurrió un error al actualizar la descripción del grupo.*');
   }
 };
 
