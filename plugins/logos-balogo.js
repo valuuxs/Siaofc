9a226aca890c6c8d036396707de5aede648e9274
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-18 22:01:53 -0500

    Actualizar logos-balogo.js

--- plugins/logos-balogo.js
+++ plugins/logos-balogo.js
@@ -1,5 +1,5 @@
-let handler = async (m, { conn, args }) => {
-  if (args.length < 2) return m.reply('Ejemplo : balogo txt1 txt2');
+let handler = async (m, { conn, usedPrefix, command, args }) => {
+  if (args.length < 2) return m.reply(`${xanime} Por favor ingresa el comando más el texto uno y dos.*\n> *\`Ejemplo:\`* ${usedPrefix + command} Dev Criss`);
   await conn.sendMessage(m.chat, {
     image: {
       url: 'https://api.nekorinn.my.id/maker/ba-logo?textL=' + encodeURIComponent(args[0]) + '&textR=' + encodeURIComponent(args.slice(1).join(' '))
