dc484e71a79dc75bdcb8ce7489196d0202753eed
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-18 21:52:52 -0500

    Actualizar owner-chetar.js

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -1,7 +1,7 @@
 
 const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
-        conn.sendMessage(m.chat, {text: `*[ ℹ️ ] @${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: fkontak});
+        conn.sendMessage(m.chat, {text: `${xowner} *@${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: fkontak});
       global.db.data.users[m.sender].money = Infinity;
     global.db.data.users[m.sender].diamantes = Infinity;
   global.db.data.users[m.sender].level = Infinity;
@@ -9,7 +9,7 @@ const handler = async (m, { conn }) => {
 };
 handler.help = ['cheat'];
 handler.tags = ['owner'];
-handler.command = /^(ilimitado|infiniy|chetar)$/i;
+handler.command = /^(ilimitado|infiniy|chetar|cheat)$/i;
 handler.mods = true;
 handler.fail = null;
 export default handler;
