fdc242d0d1b7867a0f17ce67be78889f2e32da9d
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-19 08:24:30 -0500

    Actualizar gc-setbye.js

--- plugins/gc-setbye.js
+++ plugins/gc-setbye.js
@@ -5,7 +5,7 @@ global.db.data.chats[m.chat].sBye = text
 conn.reply(m.chat, `*✅ La Despedida del grupo ha sido configurada*`, fkontak, m)  
 
 } else {
-    conn.reply(m.chat, `*🍃 ¡Escribe el mensaje de despedida!*\n✎ *Puedes usar:*\n\n- *\`@user\`* (Mención al Usuario)\n- *\`@group\`* (Nombre del Grupo)\n- *\`@desc\`* (Descripción del Grupo)\n\n> 🌿 Los @ son opcionales`, fkontak, m)
+    conn.reply(m.chat, `*${xgc} ¡Escribe el mensaje de despedida!*\n✎ *Puedes usar:*\n\n- *\`@user\`* (Mención al Usuario)\n- *\`@group\`* (Nombre del Grupo)\n- *\`@desc\`* (Descripción del Grupo)\n\n> 🌿 Los @ son opcionales`, fkontak, m)
 }
 }
 
