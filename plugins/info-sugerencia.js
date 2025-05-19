a6830b4f94eb7d7a7925a51ecd29fe75856f862c
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-18 22:46:49 -0500

    Actualizar info-sugerencia.js

--- plugins/info-sugerencia.js
+++ plugins/info-sugerencia.js
@@ -5,16 +5,16 @@ let handler = async (m, { conn, text }) => {
     const user = m.pushName || 'Usuario';
 
     if (!text) {
-        return conn.reply(m.chat, `*[ ℹ️ ] ¡Hola ${user}! ¿Qué comando quieres sugerir?*`, m);
+        return conn.reply(m.chat, `*${xinfo} ¡Hola! ${user} ¿Qué comando desea sugerir?*`, m);
     }
-    if (text.length < 10) {
-        return conn.reply(m.chat, '*[ ⚠️ ] La sugerencia debe tener al menos 10 caracteres.*', m);
+    if (text.length < 5) {
+        return conn.reply(m.chat, '*⚠️ La sugerencia debe tener al menos 5 caracteres.*', m);
     }
     if (text.length > 1000) {
-        return conn.reply(m.chat, '*[ ⚠️ ] El máximo permitido para la sugerencia es de 1000 caracteres.*', m);
+        return conn.reply(m.chat, '*⚠️ El máximo permitido para la sugerencia es de 1000 caracteres.*', m);
     }
 
-    const teks = `*[ 💡 ] Sugerencia de nuevo comando de ${user}:*
+    const teks = `*💡 Sugerencia de un nuevo comando de ${user}:*
 
 *📝 Comando sugerido:*
 > ${text}`;
@@ -23,7 +23,7 @@ let handler = async (m, { conn, text }) => {
         mentions: conn.parseMention(teks),
     });
 
-    m.reply('*[ ✅ ] ¡Tu sugerencia ha sido enviada al propietario!* Gracias por tu aporte.');
+    m.reply('*✅ ¡Tu sugerencia ha sido enviada al propietario! Gracias por tu aporte.*');
 };
 
 handler.help = ['newcommand <texto>'];
