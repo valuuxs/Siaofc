b6812d3aba158e5bca9a3b540d8ce48f535bb698
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-18 22:51:39 -0500

    Actualizar info-reporte.js

--- plugins/info-reporte.js
+++ plugins/info-reporte.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     try {
         text = text.trim();
-        if (!text) return m.reply('*[ ℹ️ ] Ingrese el error que desea reportar.*');
-        if (text.length < 10) return m.reply('*[ ⚠️ ] Especifique bien el error, mínimo 10 caracteres.*');
-        if (text.length > 1000) return m.reply('*[ ⚠️ ] Máximo 1000 caracteres para enviar el error.*');
+        if (!text) return m.reply(`*${xinfo} Por favor ingresa el error que desea reportar.*`);
+        if (text.length < 10) return m.reply('*⚠️ Especifique bien el error, mínimo 10 caracteres.*');
+        if (text.length > 1000) return m.reply('*⚠️ Máximo 1000 caracteres para enviar el error.*');
 
         const sender = m.sender.split('@')[0];
         const teks = `
@@ -20,10 +20,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
             mentions: conn.parseMention(teks),
         });
 
-        m.reply(`*[ ✅ ] ${m.pushName || 'Usuario'}, tu reporte ha sido enviado a mi creador. Recuerda que los informes falsos pueden ocasionar baneo.*`);
+        m.reply(`*✅ ${m.pushName || 'Usuario'}, tu reporte ha sido enviado a mi creador. Recuerda que los informes falsos pueden ocasionar baneo.*`);
     } catch (err) {
         console.error(err);
-        m.reply('*[ ❌ ] Ocurrió un error al enviar tu reporte. Inténtalo de nuevo más tarde.*');
+        m.reply('*✖️ Ocurrió un error al enviar tu reporte. Inténtalo de nuevo más tarde.*');
     }
 };
 
