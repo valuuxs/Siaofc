cbb99f01172bdff8330924fd94c7694fbe25c5be
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-18 19:39:39 -0500

    Actualizar owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '*⚠️ Este chat no está registrado*', m)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '*chat no baned', m)
+if (!chat.isBanned) return conn.reply(m.chat, '*🍚 Este chat no esta baneado *', m)
 chat.isBanned = false
-await conn.reply(m.chat, `Activado`, m)
+await conn.reply(m.chat, `*${xowner} Shadow Ultra se activó para este chat.*`, m)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
-handler.command = ['unbanchat','desbanearchat','desbanchat']
+handler.command = ['unbanchat','desbanearchat','desbanchat', 'activar']
 handler.rowner = true
 handler.group = true
 
