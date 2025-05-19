27092ba08d8dfe03d2c98baa897deb05df12e872
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-19 07:51:13 -0500

    Actualizar game-delttt.js

--- plugins/game-delttt.js
+++ plugins/game-delttt.js
@@ -1,9 +1,9 @@
 import MessageType from '@whiskeysockets/baileys';
 const handler = async (m, {conn, usedPrefix, command}) => {
   const room = Object.values(conn.game).find((room) => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender));
-  if (room == undefined) return conn.sendButton(m.chat, '*[ ℹ️ ] No estas en ninguna partida de tres en raya.*', wm, null, [['Iniciar sala de juego', `${usedPrefix}ttt partida nueva`]], m);
+  if (room == undefined) return conn.sendButton(m.chat, `*${xgame} No estas en ninguna partida de tres en raya.*`, wm, null, [['Iniciar sala de juego', `${usedPrefix}ttt partida nueva`]], m);
   delete conn.game[room.id];
-  await m.reply('*[ ✔ ] Se eliminó la sala del juego de tres en raya.*');
+  await m.reply('✔ Se eliminó la sala del juego de tres en raya.*');
 };
 handler.command = /^(delttt|deltt|delxo|deltictactoe)$/i;
 handler.fail = null;
