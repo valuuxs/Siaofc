2a11f878e6e22df13f423e17d78e24cd4f607f69
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-19 08:38:26 -0500

    Actualizar herramientas-readViewOnce.js

--- plugins/herramientas-readViewOnce.js
+++ plugins/herramientas-readViewOnce.js
@@ -5,7 +5,7 @@ let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
 if (!m.quoted) return conn.reply(m.chat, `*🧋 Por favor, responde a una imagen ViewOnce (ver una sola vez)*.`, m)
-if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `*🧋 Responde a una imagen ViewOnce (ver una sola vez)*`, m)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `*${xtools} Responde a una imagen ViewOnce (ver una sola vez)*`, m)
 let buffer = await m.quoted.download(false);
 if (/videoMessage/.test(m.quoted.mtype)) {
 return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
