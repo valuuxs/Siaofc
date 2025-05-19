32ad5728ed5248fa8b0c6e95b6b54a2a2f3dbe21
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-19 07:56:35 -0500

    Actualizar gc-admins.js

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -5,7 +5,7 @@ const handler = async (m, {conn, participants, groupMetadata, args}) => {
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
   const pesan = args.join` `;
   const oi = `${pesan}`;
-  const text = `*🥯 \`Invocando Admins\`*
+  const text = `*${xgroup} \`Invocando Admins\`*
   
 ${listAdmin}
 
