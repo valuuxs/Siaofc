0cb75e1f6a59e60415f5cb091d645842e2c3cab2
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-18 19:42:47 -0500

    Actualizar owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m) => {
 
 global.db.data.chats[m.chat].isBanned = true
-conn.reply(m.chat, `*🍚 Shadow Ultra se desactivó para este chat.*`, m)
+conn.reply(m.chat, `*${xowner} Shadow Ultra se desactivó para este chat.*`, m)
 
 }
 handler.help = ['banchat']
