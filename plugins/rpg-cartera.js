0cd9c3c2ff7aa12c2a638a64db99134b77830417
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-18 20:17:18 -0500

    Actualizar rpg-cartera.js

--- plugins/rpg-cartera.js
+++ plugins/rpg-cartera.js
@@ -1,9 +1,9 @@
 let handler = async (m, {conn, usedPrefix}) => {
 let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-if (who == conn.user.jid) return m.reply('*[ ⚠️ ] No puedes consultar el saldo del bot.*')
-if (!(who in global.db.data.users)) return conn.reply(m.chat, '*[ ⚠️ ] El usuario no se encuentra en mi base de Datos.*', m)
+if (who == conn.user.jid) return m.reply('*⚠️ No puedes consultar el saldo del bot.*')
+if (!(who in global.db.data.users)) return conn.reply(m.chat, '*⚠️ El usuario no se encuentra en mi base de Datos.*', m)
 let user = global.db.data.users[who]
-await m.reply(`${who == m.sender ? `*[ ℹ️ ] Tienes \`${user.diamantes}\` Diamantes 💎 en tu Cartera*` : `*[ ℹ️ ] El usuario @${who.split('@')[0]} tiene \`${user.diamantes}\` Diamantes 💎 en su Cartera*`}. `, null, { mentions: [who] })}
+await m.reply(`${who == m.sender ? `*${xrpg} Tienes \`${user.diamantes}\` Diamantes 💎 en tu Cartera*` : `*${xrpg} El usuario @${who.split('@')[0]} tiene \`${user.diamantes}\` Diamantes 💎 en su Cartera*`}. `, null, { mentions: [who] })}
 
 handler.help = ['estrellas']
 handler.tags = ['rpg']
