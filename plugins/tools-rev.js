a6cb571106e4e1c723b1b474736baad0fcdd1f63
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-23 15:56:30 -0500

    Actualizar plugins

---
+++ plugins/tools-rev.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+
+var handler = async (m, { usedPrefix, command }) => {
+    try {
+        await m.react('⌛'); 
+        conn.sendPresenceUpdate('composing', m.chat);
+
+        const pluginsDir = './plugins';
+
+        const files = fs.readdirSync(pluginsDir).filter(file => file.endsWith('.js'));
+
+        let response = `📂 *Revisión de Syntax Errors:*\n\n`;
+        let hasErrors = false;
+
+        for (const file of files) {
+            try {
+                await import(path.resolve(pluginsDir, file));
+            } catch (error) {
+                hasErrors = true;
+                response += `🔴 *Error en:* ${file}\n${error.message}\n\n`;
+            }
+        }
+
+        if (!hasErrors) {
+            response += '*✅ Todos los plugins están en orden.*';
+        }
+
+        await conn.reply(m.chat, response, m);
+        await m.react('✅');
+    } catch (err) {
+        await m.react('✖️'); 
+        console.error(err);
+        conn.reply(m.chat, '✖️ *Ocurrió un fallo al verificar los plugins.*', m);
+    }
+};
+
+handler.command = ['rev'];
+handler.help = ['rev'];
+handler.tags = ['tools'];
+
+export default handler;
