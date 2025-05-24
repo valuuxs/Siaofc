69db6fde5695c0d2b530fd7e9aa3fc23388efee1
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-24 13:41:09 -0500

    Actualizar sticker-bratip.js

--- plugins/sticker-bratip.js
+++ plugins/sticker-bratip.js
@@ -1,19 +1,23 @@
 import { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-
   try {
-if (!text) {
-    return conn.reply(m.chat, `*${usedPrefix + command} Por favor, ingresa un texto para realizar tu sticker.*`, m, rcanal)
-}
+    if (!text) {
+      return conn.reply(m.chat, `*${xsticker} Por favor, ingresa un texto para realizar tu sticker.*`, m, rcanal)
+    }
+
+    await m.react(xsticker)
+
     const url = `https://api.nekorinn.my.id/maker/brat-v2?text=${encodeURIComponent(text)}`
     const stiker = await sticker(null, url, packname, author)
 
     if (!stiker) throw 'Error al generar el sticker.'
 
     await conn.sendFile(m.chat, stiker, 'sticker.webp', '', fkontak)
+    //await m.react('✅')
   } catch (err) {
     console.error(err)
+    await m.react('✖️')
     m.reply(typeof err === 'string' ? err : 'Ocurrió un error al generar el sticker.')
   }
 }
