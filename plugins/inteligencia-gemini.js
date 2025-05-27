51f5b01d3ee7e9dfcc1a69d6177fc04e1474212c
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-26 22:21:38 -0500

    Actualizar inteligencia-gemini.js

--- plugins/inteligencia-gemini.js
+++ plugins/inteligencia-gemini.js
@@ -1,16 +1,22 @@
 import fetch from 'node-fetch'
-var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `*${xia} Ingrese una petición para que Gemini lo responda.*`, m)
-try {
 
+var handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) return conn.reply(m.chat, `*${xia} Ingrese una petición para que Gemini lo responda.*`, m)
+  try {
     conn.sendPresenceUpdate('composing', m.chat) // Writing
-var apii = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?text=${text}`)
-var res = await apii.json()
-await m.reply(res.result)
-} catch {
-await m.react('✖️')
-await conn.reply(m.chat, `*✖️ Gemini no puede responder a esa pregunta.*`, m)
-}}
+
+    const apii = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?text=${encodeURIComponent(text)}`)
+    const res = await apii.json()
+
+    if (!res.result) throw 'Respuesta vacía o incorrecta'
+    await m.reply(res.result)
+  } catch (err) {
+    console.error(err)
+    await m.react('✖️')
+    await conn.reply(m.chat, `*✖️ Gemini no puede responder a esa pregunta.*`, m)
+  }
+}
+
 handler.command = ['gemini']
 handler.help = ['gemini']
 handler.tags = ['ai']
