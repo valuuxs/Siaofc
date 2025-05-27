0aa7c74a90af1059ca0caf317f2e53351bdba8f0
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-26 22:54:01 -0500

    Actualizar _test2.js

--- plugins/_test2.js
+++ plugins/_test2.js
@@ -1,4 +1,4 @@
-const handler = async (m, { conn, text }) => {
+/*const handler = async (m, { conn, text }) => {
   if (!text) throw '*[❗] Ingresa el mensaje a enviar con la ubicación*';
 
   const mensaje = '[❗𝐋𝐈𝐕𝐄 𝐓𝐄𝐒𝐓❗]\n\n' + text + '\n\nEste es un test de liveLocationMessage';
@@ -23,4 +23,52 @@ handler.tags = ['test'];
 handler.command = /^testlive$/i;
 handler.owner = true;
 
+export default handler;*/
+import { randomBytes } from "crypto"
+import axios from "axios"
+
+let handler = async (m, { conn, text }) => {
+    if (!text) throw `${emoji} ¿Cómo puedo ayudarte hoy?`;
+    try {
+        conn.reply(m.chat, m);
+        let data = await chatGpt(text);
+        await conn.sendMessage(m.chat, { 
+            text: '*Demo:* ' + data
+        }, { quoted: m });
+
+    } catch (err) {
+        m.reply('error cik:/ ' + err);
+    }
+}
+
+handler.help = ['demo *<texto>*'];
+handler.command = ['demo', 'openai'];
+handler.tags = ['ai'];
+handler.group = true;
+
 export default handler;
+
+async function chatGpt(query) {
+    try {
+        const { id_ } = (await axios.post("https://chat.chatgptdemo.net/new_chat", { user_id: "crqryjoto2h3nlzsg" }, { headers: { "Content-Type": "application/json" } })).data;
+
+        const json = { "question": query, "chat_id": id_, "timestamp": new Date().getTime() };
+
+        const { data } = await axios.post("https://chat.chatgptdemo.net/chat_api_stream", json, { headers: { "Content-Type": "application/json" } });
+        const cek = data.split("data: ");
+
+        let res = [];
+
+        for (let i = 1; i < cek.length; i++) {
+            if (cek[i].trim().length > 0) {
+                res.push(JSON.parse(cek[i].trim()));
+            }
+        }
+
+        return res.map((a) => a.choices[0].delta.content).join("");
+
+    } catch (error) {
+        console.error("Error parsing JSON:", error);
+        return 404;
+    }
+}
