28b3ff92dc428deb00b3e2c0d551780cebfe0c9b
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-05-28 23:41:19 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -33,7 +33,7 @@ if (chat.welcome && m.messageStubType == 27) {
         .replace(/@group/g, groupName)
         .replace(/@desc/g, groupDesc)
     : `*¡Bienvenido(a)!*\n෫ࣲׄ֟፝͡${taguser} ☕꒱\n\nᦷᩘᦷ   ݂ 𝖣𝗂𝗌𝖿𝗋𝗎𝗍𝖺 𝖽𝖾 𝗍𝗎 𝖾𝗌𝗍𝖺𝖽𝗂𝖺.\n✎ 𝖴𝗌𝖺 *#help* 𝗉𝖺𝗋𝖺 𝗏𝖾𝗋 𝗅𝖺 𝗅𝗂𝗌𝗍𝖺 𝖽𝖾 𝖼𝗈𝗆𝖺𝗇𝖽𝗈𝗌.\n\n> ${dev}`
-  await conn.sendBot(m.chat, txt, sunflare, bienvenida, img, img, insta, fkontak)
+  await conn.sendMini(m.chat, txt, sunflare, bienvenida, img, img, insta, fkontak)
 }
 
 if (chat.welcome && m.messageStubType == 28) {
@@ -46,7 +46,7 @@ if (chat.welcome && m.messageStubType == 28) {
         .replace(/@group/g, groupName)
         .replace(/@desc/g, groupDesc)
     : `*¡Expulsado!*\n෫ࣲׄ֟፝͡${taguser} 👊🏻꒱\n\nᏊ⁠ 𝖴𝗇 𝗇𝖾𝗀𝗋𝗈 𝗆𝖾𝗇𝗈𝗌 𝖾𝗇 𝖾𝗅 𝗀𝗋𝗎𝗉𝗈, 𝗉𝗈𝗋 𝗇𝗈 𝗈𝖻𝖾𝖽𝖾𝖼𝖾𝗋 𝗅𝖺𝗌 𝗋𝖾𝗀𝗅𝖺𝗌.\n ׅ⿻ 𝖮𝗃𝖺𝗅𝖺 𝗒 𝗅𝖺 𝖾𝗅𝗂𝗆𝗂𝗇𝖺𝖼𝗂𝗈𝗇 𝗅𝖾 𝗁𝖺𝗀𝖺 𝗋𝖾𝖿𝗅𝖾𝗑𝗂𝗈𝗇𝖺𝗋 𝗑𝖣\n\n> ${dev}`    
-    await conn.sendBot(m.chat, txt1, sunflare1, ban, img, img, insta, fkontak)
+    await conn.sendMini(m.chat, txt1, sunflare1, ban, img, img, insta, fkontak)
   }
 
 if (chat.welcome && m.messageStubType == 32) {
@@ -59,5 +59,5 @@ if (chat.welcome && m.messageStubType == 32) {
         .replace(/@group/g, groupName)
         .replace(/@desc/g, groupDesc)
     : `*¡Hasta Luego!*\n෫ࣲׄ֟፝͡${taguser} 👋🏻꒱\n\nᏊ⁠˘⁠ ⁠ꈊ⁠ ⁠˘⁠ ⁠Ꮚ 𝖦𝗋𝖺𝖼𝗂𝖺𝗌 𝗉𝗈𝗋 𝖾𝗌𝗍𝖺𝗋 𝖺𝗊𝗎𝗂. 𝖳𝗎 𝗉𝗋𝖾𝗌𝖾𝗇𝖼𝗂𝖺 𝖿𝗎𝖾 𝗎𝗇 𝖾𝗌𝗍𝗈𝗋𝖻𝗈.\n ׅ⿻ 𝖮𝗃𝖺𝗅𝖺 𝗒 𝗌𝖾 𝗅𝗈 𝗏𝗂𝗈𝗅𝖾𝗇 𝗅𝗈𝗌 𝗇𝖾𝗀𝗋𝗈𝗌 𝗉𝗈𝗋 𝖺𝗅𝗍𝖺 𝗉𝗎𝗍𝖺.\n\n> ${dev}`
-    await conn.sendBot(m.chat, txt1, sunflare2, bye, img, img, insta, fkontak)
+    await conn.sendMini(m.chat, txt1, sunflare2, bye, img, img, insta, fkontak)
   }}
