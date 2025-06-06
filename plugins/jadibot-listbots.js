10b87d5aa5a1c909563bc4e6d9b84217ec3bb9ae
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-06 11:27:26 -0500

    Actualizar plugins

---
+++ plugins/jadibot-listbots.js
@@ -0,0 +1,25 @@
+import ws from 'ws'
+
+let handler = async (m, { conn }) => {
+   let uniqueUsers = new Map()
+
+   if (!global.conns || !Array.isArray(global.conns)) {
+     global.conns = []
+   }
+
+   global.conns.forEach((conn) => {
+     if (conn.user && conn.ws?.socket?.readyState !== ws.CLOSED) {
+       uniqueUsers.set(conn.user.jid, conn)
+     }
+   })
+
+   let totalUsers = uniqueUsers.size
+   let txt = '*`🌿 Subbots Activos:`*' + ` » *${totalUsers || 0}*`
+
+   await conn.reply(m.chat, txt, m, rcanal)
+}
+
+handler.command = ['listjadibot', 'bots', 'listbots']
+handler.help = ['bots']
+handler.tags = ['serbot']
+export default handler
