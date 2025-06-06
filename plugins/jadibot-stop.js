8817301d49b3a41ebdacac5337310a545a610fd3
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-06 12:21:12 -0500

    Actualizar plugins

---
+++ plugins/jadibot-stop.js
@@ -0,0 +1,13 @@
+
+let handler  = async (m, { conn }) => {
+if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, `*☁️ El bot principal no se puede apagar.*`, m, rcanal)
+else {
+await conn.reply(m.chat, `*El Subbot fue desactivado exitosamente*`, m)
+conn.ws.close()
+}}
+handler.command = handler.help = ['stop', 'byebot'];
+handler.tags = ['serbot'];
+handler.owner = true
+handler.private = true
+handler.register = true
+export default handler
