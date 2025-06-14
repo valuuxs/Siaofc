b112f9352c023e41610f4e7f63564a2a179ef073
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-13 20:11:40 -0500

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,11 +1,20 @@
-const handler = async (m, {conn}) => {
-  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'error.png', '*🏳️‍🌈 Miren a este gay 🏳️‍🌈*', m);
+const handler = async (m, { conn }) => {
+  const who = m.mentionedJid && m.mentionedJid[0]
+    ? m.mentionedJid[0]
+    : m.fromMe
+    ? conn.user.jid
+    : m.sender;
+
+  const avatarUrl = await conn.profilePictureUrl(who, 'image')
+    .catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
+
+  const apiUrl = `https://some-random-api.com/canvas/gay?avatar=${encodeURIComponent(avatarUrl)}`;
+
+  await conn.sendFile(m.chat, apiUrl, 'gay.png', '*🏳️‍🌈 Miren a este gay 🏳️‍🌈*', m);
 };
 
 handler.help = ['gay'];
 handler.tags = ['maker'];
 handler.command = /^(gay2|gayp)$/i;
+
 export default handler;
