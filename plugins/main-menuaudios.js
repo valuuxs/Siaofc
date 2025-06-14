af10504c0a7d274886ffb4fbbbce57e2aa18bf5d
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-14 16:08:28 -0500

    Actualizar main-menuaudios.js

--- plugins/main-menuaudios.js
+++ plugins/main-menuaudios.js
@@ -8,7 +8,7 @@ const handler = async (m, { conn, usedPrefix, isPrems }) => {
     let insta = 'https://instagram.com/dev.criss_vx';
     const shadow = 'Menú Owner';
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    const txt = '${await conn.getName(m.sender)}, Welcome to my developer menu, follow me on Instagram, thank you very much.';
+    const txt = `${await conn.getName(m.sender)}, Welcome to my developer menu, follow me on Instagram, thank you very much.`;
 
     const text = `
  ꡴ㅤ   ︵ᤢ⏜   ᷃ᩚ   ☕᪶     ᷃ᩚ ⏜ᤢ︵    ㅤ᪬
