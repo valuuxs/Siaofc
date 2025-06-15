1bae5ae55af2782daad91403f72ae8b888725d19
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-15 16:19:48 -0500

    Actualizar owner-getplugin.js

--- plugins/owner-getplugin.js
+++ plugins/owner-getplugin.js
@@ -24,12 +24,12 @@ const handler = async (m, { conn, isROwner, usedPrefix, command, text }) => {
   } finally {
     const { stdout, stderr } = o;
     if (stdout.trim()) {
-      // const aa = await conn.sendMessage(m.chat, { text: stdout }, { quoted: m });
-      await conn.sendMessage(m.chat, { document: fs.readFileSync(`./plugins/${text}.js`), mimetype: 'application/javascript', fileName: `${text}.js` }, { quoted: m });
+       const aa = await conn.sendMessage(m.chat, { text: stdout }, { quoted: m });
+     // await conn.sendMessage(m.chat, { document: fs.readFileSync(`./plugins/${text}.js`), mimetype: 'application/javascript', fileName: `${text}.js` }, { quoted: m });
     }
     if (stderr.trim()) {
-      // const aa2 = await conn.sendMessage(m.chat, { text: stderr }, { quoted: m });
-      await conn.sendMessage(m.chat, { document: fs.readFileSync(`./plugins/${text}.js`), mimetype: 'application/javascript', fileName: `${text}.js` }, { quoted: m });
+       const aa2 = await conn.sendMessage(m.chat, { text: stderr }, { quoted: m });
+    //  await conn.sendMessage(m.chat, { document: fs.readFileSync(`./plugins/${text}.js`), mimetype: 'application/javascript', fileName: `${text}.js` }, { quoted: m });
     }
   }
 };
