81b1bc5cb1364d0c280a6ab00d9ce73bb232b7a2
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-15 17:05:10 -0500

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -8,7 +8,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
 const oi = `*\`AVISO:\`* ${pesan}`;
-  let teks = `𝗦𝗛𝗔𝗗𝗢𝗪 𝗕𝗢𝗧 𝗟𝗢𝗦 𝗜𝗡𝗩𝗢𝗖𝗔 🙂‍↕\n*INTEGRANTES:* \`${participants.length}\`\n\n ${grupo}\n\n ${oi}\n\nෆ *ETIQUETAS*\n`;
+  let teks = `𝗦𝗛𝗔𝗗𝗢𝗪 𝗕𝗢𝗧 𝗟𝗢𝗦 𝗜𝗡𝗩𝗢𝗖𝗔 🙂‍↕\n*INTEGRANTES:* \`${participants.length}\`\n\n ${canal}\n\n ${oi}\n\nෆ *ETIQUETAS*\n`;
   for (const mem of participants) {
     teks += `യ ׄ🍁˚ @${mem.id.split('@')[0]}\n`;
   }
