ffa21a9cf3cbc661ee75ca128068de91673771b2
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-15 17:27:08 -0500

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -8,9 +8,9 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
 const oi = `*\`AVISO:\`* ${pesan}`;
-  let teks = `𝗗𝗘𝗦𝗣𝗜𝗘𝗥𝗧𝗘𝗡 𝗣𝗟𝗔𝗡𝗧𝗔𝗦 🌱\n> \`𝖨𝗇𝗍𝖾𝗀𝗋𝖺𝗇𝗍𝖾𝗌:\` *${participants.length}*\n\n ${channel}\n\n ${oi}\n\nෆ *ETIQUETAS*\n`;
+  let teks = `𝗗𝗘𝗦𝗣𝗜𝗘𝗥𝗧𝗘𝗡 𝗣𝗟𝗔𝗡𝗧𝗔𝗦 🌱\n> \`𝖨𝗇𝗍𝖾𝗀𝗋𝖺𝗇𝗍𝖾𝗌:\` *${participants.length}*\n\n ${channel}\n\n ${oi}\n\n  ━━ *_ETIQUETAS_*\n`;
   for (const mem of participants) {
-    teks += `യ ׄ🌿˚ @${mem.id.split('@')[0]}\n`;
+    teks += `യ ׄ🥞˚ @${mem.id.split('@')[0]}\n`;
   }
   teks += `> ${club}`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
