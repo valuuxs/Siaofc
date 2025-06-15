0109c1c925a93452fb9f7de3561abbe3f9f66f5b
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-14 23:37:06 -0500

    Actualizar owner-creargc.js

--- plugins/owner-creargc.js
+++ plugins/owner-creargc.js
@@ -1,10 +1,11 @@
 
 let handler = async (m, { conn, text }) => {
 if (!text) return m.reply(`*${xowner} Ingresa un nombre para crear el grupo.*`)
+
 try{
-m.reply(`*☁️ Creando Grupo...*`)
 let group = await conn.groupCreate(text, [m.sender])
 let link = await conn.groupInviteCode(group.gid)
+m.reply(`*☁️ Grupo creado con éxito.*`)
 m.reply('https://chat.whatsapp.com/' + url)
 } catch (e) {
 m.reply(`*✖️ Ocurrió un error, es posible que el grupo tenga lid activado.*`)
