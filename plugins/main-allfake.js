b9485d9d3751a8e1a6df0cf1ba7b82d4ae8cc39e
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-21 17:05:37 -0500

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -143,14 +143,16 @@ global.date = `${diaSemana}, ${fecha.format('D')} de ${mes} del ${fecha.format('
 global.hora = fecha.format('h:mm A')
 
 //TAGS & STICKERS
+global.userbot = await conn.getName(conn.user.id)
+global.tagbot = '@' + conn.user.id.split('@')[0]
 global.usname = await conn.getName(m.sender)
 global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
 
 global.packN = `協会  Sʜʌᴅᴏᴡ′s Cʟᴜʙ  🍁 ࣪ ˖ ∿
-↳ @shadows_club\n☁ 𝖡𝗈𝗍:\n↳ @Shadow Ultra\n🌺 𝖴𝗌𝗎𝖺𝗋𝗂𝗈:`;
-global.authN = `\n↳ ${usname}`;
+↳ @shadows_club\n☁ 𝖡𝗈𝗍:\n↳ ${tagbot}\n🌺 𝖴𝗌𝗎𝖺𝗋𝗂𝗈:`;
+global.authN = `\n↳ ${taguser}`;
 
 //FAKES
 global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
