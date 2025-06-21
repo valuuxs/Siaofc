1c451990ae29582b111fab555368eb551af50292
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-20 23:00:13 -0500

    Actualizar plugins

---
+++ plugins/info-grupos.js
@@ -0,0 +1,26 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+
+let grupos = `*¡Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad oficial* 🌹
+
+✑ 𝖦𝗋𝗎𝗉𝗈 𝖮𝖿𝗂𝖼𝗂𝖺𝗅 𝖲𝗁𝖺𝖽𝗈𝗐
+${grupo}
+
+✑ 𝖢𝗅𝗎𝖻/𝖢𝗈𝗆𝗆𝗎𝗇𝗂𝗍𝗒 
+   𝖲𝗁𝖺𝖽𝗈𝗐𝗌 𝖢𝗅𝗎𝖻
+${community}
+
+✑ 𝖢𝖺𝗇𝖺𝗅𝖾𝗌 𝖮𝖿𝗂𝖼𝗂𝖺𝗅𝖾𝗌
+
+
+> ${dev}`
+
+let img = 'https://cdnmega.vercel.app/media/0koXFaLT@i7K8L3tXymGVsjK7a1cLTaxk5th9cUTCpRU4FSchn-4';
+
+conn.sendMessage(m.chat, { image: { url: img }, caption: grupos }, { quoted: fkontak });
+}
+
+handler.help = ['grupos']
+handler.tags = ['info']
+handler.command = ['grupos', 'shadowgrupos', 'club']
+
+export default handler
