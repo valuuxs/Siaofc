d99aebc26e110c4bc4c87f297ba50ab8255836c0
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-29 10:28:43 -0500

    Actualizar _autoaceptar.js

--- plugins/_autoaceptar.js
+++ plugins/_autoaceptar.js
@@ -17,3 +17,34 @@ if (chat.autoAceptar && !isAdmin) {
                 await conn.groupRequestParticipantsUpdate(m.chat, [jid], "approve")}}
 }}
 export default handler*/
+
+let handler = m => m
+
+handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
+  if (!m.isGroup) return !1
+  let chat = global.db.data.chats[m.chat] || {}
+
+  if (chat.autoAceptar && !isAdmin) {
+    if (!isBotAdmin) return !0
+
+    const latinPrefix = '5' // Aceptar solo números que empiezan con 5
+    const participants = await conn.groupRequestParticipantsList(m.chat).catch(_ => [])
+
+    const filtered = participants.filter(p =>
+      p.jid.includes('@s.whatsapp.net') && p.jid.split('@')[0].startsWith(latinPrefix)
+    )
+
+    for (const participant of filtered) {
+      await conn.groupRequestParticipantsUpdate(m.chat, [participant.jid], 'approve')
+    }
+
+    if (m.messageStubType === 172 && m.messageStubParameters) {
+      const [jid] = m.messageStubParameters
+      if (jid.includes('@s.whatsapp.net') && jid.split('@')[0].startsWith(latinPrefix)) {
+        await conn.groupRequestParticipantsUpdate(m.chat, [jid], 'approve')
+      }
+    }
+  }
+}
+
+export default handler
