da7e7a0c269cdf1f8b2358a8a592d3e15ac9700a
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-29 17:49:52 -0500

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -174,7 +174,7 @@ global.urls = [
 let gifUrl = urls[Math.floor(Math.random() * urls.length)];
 
 //global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: textbot, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
-global.rcanal2 = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, }, }
+global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, }, }
 
 
 export default handler
