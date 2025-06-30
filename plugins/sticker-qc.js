82f98ea09fc81cce0bb2b1f426466d27d65ca65d
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-06-29 21:28:39 -0500

    Actualizar sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -6,7 +6,7 @@ if (args.length >= 1) {
 text = args.slice(0).join(" ");
 } else if (m.quoted && m.quoted.text) {
 text = m.quoted.text;
-} else return conn.reply(m.chat, `*${xsticker}Por favor, ingresa o responde a un texto para realizar tu sticker.*`, m, rcanal2);
+} else return conn.reply(m.chat, `*${xsticker}Por favor, ingresa o responde a un texto para realizar tu sticker.*`, m, rcanal);
 if (!text) return conn.reply(m.chat, `*${xsticker} Agrega un texto.*`, m);
 const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
 const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
