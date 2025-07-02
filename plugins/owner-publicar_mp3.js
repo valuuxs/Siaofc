f8bfd2730f2b26a8bba553d358fbe797b094e2cc
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-02 00:58:34 -0500

    Actualizar plugins

---
+++ plugins/owner-publicar_mp3.js
@@ -0,0 +1,39 @@
+import { toAudio } from '../lib/converter.js';
+
+const handler = async (m, { conn, usedPrefix, command }) => {
+  const canalJid = '';
+
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q || q.msg).mimetype || q.mediaType || '';
+
+  if (!/video|audio/.test(mime)) {
+    return conn.reply(m.chat, `*${xconverter} Responda al video o nota de voz con el comando .ptt para convertirlo en nota de voz.*`, m);
+  }
+
+  const media = await q.download();
+  if (!media) {
+    return conn.reply(m.chat, '⚠️ Ocurrió un error al descargar el archivo.', m);
+  }
+
+  const audio = await toAudio(media, 'mp4');
+  if (!audio.data) {
+    return conn.reply(m.chat, '⚠️ Ocurrió un error al convertir el archivo a nota de voz.', m);
+  }
+
+  const audioMessage = {
+    audio: audio.data,
+    mimetype: 'audio/mpeg',
+    ptt: true
+  };
+
+  // 📤 Channel
+  await conn.sendMessage(canalJid, audioMessage, { quoted: fkontak });
+
+  await conn.reply(m.chat, '*✅ Audio enviado correctamente al canal.*', m);
+};
+
+handler.help = ['ptt'];
+handler.command = ['publicar', 'publicarmp3', 'publimp3'];
+handler.rowner = true;
+
+export default handler;
