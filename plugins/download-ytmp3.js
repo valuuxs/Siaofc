e43a8ce43ccc9e7152876aee4f0e7e3fb258ce47
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-06 14:57:53 -0500

    Actualizar download-ytmp3.js

--- plugins/download-ytmp3.js
+++ plugins/download-ytmp3.js
@@ -11,7 +11,7 @@ const savetube = {
     download: "/download"
   },
   headers: {
-    'accept': '*/*',
+    'accept': '🦤',
     'content-type': 'application/json',
     'origin': 'https://yt.savetube.me',
     'referer': 'https://yt.savetube.me/',
