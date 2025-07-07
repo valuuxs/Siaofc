4e11af33e2a2a80dbcdf2319ece933acb3f9a8ab
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-07 14:57:03 -0500

    Actualizar owner-publicar_channel.js

--- plugins/owner-publicar_channel.js
+++ plugins/owner-publicar_channel.js
@@ -27,7 +27,7 @@ const handler = async (m, { conn, text }) => {
 
     if (!res?.key?.id) throw '❌ El contenido no se pudo enviar (respuesta inválida).';
 
-    return conn.reply(m.chat, '*✅ Contenido enviado correctamente al canal.*', m);
+    return conn.reply(m.chat, '*✅ Contenido enviado correctamente al canal.*\nhttps://whatsapp.com/channel/0029Vafxnat8qIzxOjUrwP41', m, rcanal);
 
   } catch (e) {
     console.error('[ERROR EN PUBLICAR]:', e);
